0baf0c0a993136a3761aa52d9b31a2f3f25b7af1
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-05-15 11:02:53 +0300

    p02

---
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Lab/p01.Reverse Strings/p02.Stack Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p02.Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
