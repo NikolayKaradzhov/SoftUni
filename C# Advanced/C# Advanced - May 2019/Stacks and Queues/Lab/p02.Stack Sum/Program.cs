7cd53a8c71433c97cb064a9bfacbefa059a34111
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-05-16 11:49:30 +0300

    p02

--- C# Advanced/C# Advanced - May 2019/Stacks and Queues/Lab/p02.Stack Sum/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Lab/p02.Stack Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace p02.Stack_Sum
 {
@@ -6,7 +8,45 @@ namespace p02.Stack_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbersInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            string command = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>(numbersInput);
+
+            while (!command.Equals("end", StringComparison.InvariantCultureIgnoreCase))
+            {
+                string[] commandTokens = command.Split(" ");
+
+                if (commandTokens[0].Equals("add", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    for (int i = 1; i < commandTokens.Length; i++)
+                    {
+                        int currentNumber = int.Parse(commandTokens[i]);
+                        stack.Push(currentNumber);
+                    }
+                }
+                else if (commandTokens[0].Equals("remove", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    int countToRemove = int.Parse(commandTokens[1]);
+
+                    bool isStackCountLess = stack.Count < countToRemove;
+
+                    if (isStackCountLess)
+                    {
+
+                    }
+                    else
+                    {
+                        for (int i = 0; i < countToRemove; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
         }
     }
 }
