899a95c180d05799cfd05f1dc5d782bd491b56d0
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-16 21:55:30 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Lab/p07.Hot Potato/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p07.Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kidsInput = Console.ReadLine().Split(" ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>(kidsInput);
+
+            while (queue.Count > 1)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    string player = queue.Dequeue();
+                    queue.Enqueue(player);
+                }
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Lab/p08.Traffic Jam/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p08.Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            Queue<string> queue = new Queue<string>();
+
+            int passedCarsCounter = 0;
+            int currentCount = 0;
+
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    if (queue.Count > n)
+                    {
+                        currentCount = n;
+                    }
+                    else
+                    {
+                        currentCount = queue.Count;
+                    }
+                    for (int i = 0; i < currentCount; i++)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        passedCarsCounter++;
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{passedCarsCounter} cars passed the crossroads.");
+        }
+    }
+}
