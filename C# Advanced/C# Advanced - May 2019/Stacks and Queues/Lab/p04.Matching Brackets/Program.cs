7ec5797888a60a44608c54919642fd87dc6b5df9
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-16 20:13:13 +0300

    p04

---
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Lab/p04.Matching Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p04.Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>(); 
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int indexOfLastParenthesis = stack.Pop();
+                    Console.WriteLine(input.Substring(indexOfLastParenthesis, i - indexOfLastParenthesis + 1));
+                }
+            }
+        }
+    }
+}
