d030d865c4475843c202a0143ddac2f104a6dbf1
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-16 22:43:46 +0300

    .

--- C# Advanced/C# Advanced - May 2019/Stacks and Queues/Exercise/p01.Basic Stack Operations/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Exercise/p01.Basic Stack Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace p01.Basic_Stack_Operations
 {
@@ -6,7 +8,52 @@ namespace p01.Basic_Stack_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] checkElementsInStack = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] stackElements = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            int elementsToPush = checkElementsInStack[0];
+            int elementsToPop = checkElementsInStack[1];
+            int numberToCheckIfContains = checkElementsInStack[2];
+            int minNumber = int.MaxValue;
+
+            for (int i = 0; i < elementsToPush; i++)
+            {
+                stack.Push(stackElements[i]);
+            }
+
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(numberToCheckIfContains))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                foreach (var element in stack)
+                {
+                    if (element < minNumber)
+                    {
+                        minNumber = element;
+                    }
+                }
+                Console.WriteLine(minNumber);
+            }
         }
     }
 }
