135d19841ae0033c358b15ce57edeacd1fec7a84
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-16 22:43:46 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Exercise/p02.Basic Queue Operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p02.Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
