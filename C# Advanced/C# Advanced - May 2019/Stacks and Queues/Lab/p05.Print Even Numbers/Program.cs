8d271b2b3b3ab47adba58a9cc92216cc9ee70f46
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-16 20:55:02 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Lab/p05.Print Even Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p05.Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                var currentNumber = inputNumbers[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    if (i == inputNumbers.Length - 1)
+                    {
+                        queue.Enqueue(currentNumber);
+                        Console.WriteLine(queue.Peek());
+                        break;
+                    }
+                    queue.Enqueue(currentNumber);
+                    Console.Write($"{queue.Peek()}, ");
+                    queue.Dequeue();
+                }
+            }
+        }
+    }
+}
