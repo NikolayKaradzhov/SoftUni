673c9eb283347d921908e79d89e87754093b31f6
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-05-17 14:46:40 +0300

    Update Program.cs

--- C# Advanced/C# Advanced - May 2019/Stacks and Queues/Exercise/p02.Basic Queue Operations/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Stacks and Queues/Exercise/p02.Basic Queue Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace p02.Basic_Queue_Operations
 {
@@ -6,7 +8,42 @@ namespace p02.Basic_Queue_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] numbers = Console.ReadLine().Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            int elementsToEnqueue = input[0];
+            int elementsToDequeue = input[1];
+            int elementToCheckFor = input[2];
+
+            for (int i = 0; i < elementsToEnqueue; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int j = 0; j < elementsToDequeue; j++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(elementToCheckFor))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
         }
     }
 }
