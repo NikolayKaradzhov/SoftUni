f290831b09ef9ee824558d8b9165d7add8f6904c
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-19 16:40:28 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Multidimentional Arrays/Lab/p01.Sum Matrix Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p01.Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
