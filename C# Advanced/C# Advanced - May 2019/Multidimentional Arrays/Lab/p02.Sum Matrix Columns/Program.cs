cdbbd1415500713d75349eb8a6c957e625b1afc4
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-19 18:26:17 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Multidimentional Arrays/Lab/p02.Sum Matrix Columns/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace p02.Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[input[0],input[1]];
+            int sumOfCurrentCol = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = numbers[cols];
+                }
+            }
+            //Calculate sum of columns
+            for (int cols = 0; cols < matrix.GetLength(1); cols++)
+            {
+                sumOfCurrentCol = 0;
+                
+                for (int rows = 0; rows < matrix.GetLength(0); rows++)
+                {
+                    sumOfCurrentCol += matrix[rows, cols];
+                }
+                Console.WriteLine(sumOfCurrentCol);
+            }
+        }
+    }
+}
