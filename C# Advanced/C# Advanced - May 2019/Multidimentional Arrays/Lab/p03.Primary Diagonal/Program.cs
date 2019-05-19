cda0e5fb72a57e26a8c93a8e9adf103c25186ca7
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-20 00:02:12 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Multidimentional Arrays/Lab/p03.Primary Diagonal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace p03.Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfSquare = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfSquare, sizeOfSquare];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            int result = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        result += matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
