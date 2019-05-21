f41b350893272f9606c9e46531a1bdbf8949f55c
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-21 21:18:49 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Multidimentional Arrays/Exercise/p02.2x2 Squares in Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace p02._2x2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] text = Console.ReadLine().Split();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                }
+            }
+        }
+    }
+}
