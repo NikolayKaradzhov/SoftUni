2005fc5be93591d95e8a571bdf37737c77cd45a7
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-21 23:08:58 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Multidimentional Arrays/Exercise/p05.Snake Moves/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace p05.Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            string textInput = Console.ReadLine();
+
+            char[,] matrix = new char[rows, cols];
+
+            int counter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    //Example: SoftUni
+                    //If counter == SoftUni.Length, start from S
+                    if (counter >= textInput.Length)
+                    {
+                        counter = 0;
+                    }
+                    matrix[row, col] = textInput[counter++];
+                }
+            }
+            //Print the snake
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Multidimentional Arrays/Exercise/p06.Bomb The Basement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p06.Bomb_The_Basement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
