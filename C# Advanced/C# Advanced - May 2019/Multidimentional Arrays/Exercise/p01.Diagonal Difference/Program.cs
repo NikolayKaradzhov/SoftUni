341b1210a9f468a7f24e3d76f0c8fbe844e4bfbe
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-21 20:23:50 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Multidimentional Arrays/Exercise/p01.Diagonal Difference/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace p01.Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            int primaryDiagonalSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        primaryDiagonalSum += matrix[row, col];
+                    }
+                }
+            }
+
+            int secondaryDiagonalSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if ((row + col) == (matrixSize - 1))
+                    {
+                        secondaryDiagonalSum += matrix[row, col];
+                    }
+                }
+            }
+
+            int totalEndSum = Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+            Console.WriteLine(totalEndSum);
+        }
+    }
+}
