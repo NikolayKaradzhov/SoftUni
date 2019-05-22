7a3d356af4bff8d3333ceb71e1db3f4441c4b598
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-22 23:27:22 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Sets And Dictionaries Advanced/Lab/p01.Count Same Values in Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p01.Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (var item in input)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 1);
+                }
+                else if (dict.ContainsKey(item))
+                {
+                    dict[item] += 1;
+                }
+            }
+
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Sets And Dictionaries Advanced/Lab/p03.Product Shop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p03.Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
