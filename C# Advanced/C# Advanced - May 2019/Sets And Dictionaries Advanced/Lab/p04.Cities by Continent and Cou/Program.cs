d6c5062f1a5dceee6ae3dfcd533e0806573f65ba
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-05-23 13:39:20 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Sets And Dictionaries Advanced/Lab/p04.Cities by Continent and Cou/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p04.Cities_by_Continent_and_Cou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var database = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+
+                string[] tokens = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = tokens[0];
+                string country = tokens[1];
+                string city = tokens[2];
+
+                if (!database.ContainsKey(continent))
+                {
+                    database[continent] = new Dictionary<string, List<string>>();
+                }
+                if(!database[continent].ContainsKey(country))
+                {
+                    database[continent][country] = new List<string>();
+                }
+                database[continent][country].Add(city);
+            }
+            foreach (var kvp in database)
+            {
+                var continent = kvp.Key;
+
+                Console.WriteLine($"{continent}:");
+
+                foreach (var item in kvp.Value)
+                {
+                    var country = item.Key;
+                    List<string> cities = item.Value;
+
+                    Console.Write($"{country} -> {string.Join(", ", cities)}");
+
+                  
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
