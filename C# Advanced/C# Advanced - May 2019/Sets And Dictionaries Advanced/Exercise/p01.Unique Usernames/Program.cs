6b0bad9042947d4aceab8539ee08d1f6e068509b
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-05-23 16:10:05 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Sets And Dictionaries Advanced/Exercise/p01.Unique Usernames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p01.Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Sets And Dictionaries Advanced/Exercise/p02.Sets Of Elements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p02.Sets_Of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> firstHashSet = new HashSet<int>();
+            HashSet<int> secondHashSet = new HashSet<int>();
+            HashSet<int> thirdHashSet = new HashSet<int>();
+
+            int[] input = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstHashSetLength = input[0];
+            int secondHashSetLength = input[1];
+
+            for (int i = 0; i < firstHashSetLength + secondHashSetLength; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (i < firstHashSetLength)
+                {
+                    firstHashSet.Add(number);
+                }
+                else
+                {
+                    secondHashSet.Add(number);
+                }
+            }
+            foreach (var number in firstHashSet)
+            {
+                if (firstHashSet.Contains(number) && secondHashSet.Contains(number))
+                {
+                    Console.Write($"{number} ");
+                }   
+            }
+            Console.WriteLine();
+        }
+    }
+}
