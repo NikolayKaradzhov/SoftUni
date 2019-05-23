6f3793eda4c458b5057529826d7eae623f1f8299
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-05-23 16:17:42 +0300

    p03

---
+++ C# Advanced/C# Advanced - May 2019/Sets And Dictionaries Advanced/Exercise/p03.Periodic Table/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p03.Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> chemicalCompounds = new HashSet<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] chemicals = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < chemicals.Length; j++)
+                {
+                    chemicalCompounds.Add(chemicals[j]);
+                }
+            }
+
+            var orderedChemicals = chemicalCompounds.OrderBy(x => x);
+
+            foreach (var chemical in orderedChemicals)
+            {
+                Console.Write($"{chemical} ");
+            }
+        }
+    }
+}
