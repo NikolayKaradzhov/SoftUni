ecc53d11a3da7cd5c7445292a040851419770c43
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-28 19:41:46 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Streams, Files And Directories/Lab/p01.Odd Lines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p01.Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
