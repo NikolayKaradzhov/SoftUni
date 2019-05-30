a7e2d118fbe5d32b166c3a15d43f57a71b45912f
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-05-30 13:14:23 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Streams, Files And Directories/Lab/p03.Word Count/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p03.Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
