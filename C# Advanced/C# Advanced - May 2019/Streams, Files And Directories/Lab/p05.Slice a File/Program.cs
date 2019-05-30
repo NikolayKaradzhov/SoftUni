46f216e1c89bc0ef08cd878368357cc353cd87a2
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-30 20:05:45 +0300

    p05

--- C# Advanced/C# Advanced - May 2019/Streams, Files And Directories/Lab/p05.Slice a File/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Streams, Files And Directories/Lab/p05.Slice a File/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace p05.Slice_a_File
 {
@@ -6,7 +7,21 @@ namespace p05.Slice_a_File
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var inputFile = new FileStream(@"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Lab\Resources\05. Slice File\sliceMe.txt", FileMode.Open))
+            {
+                long fileSize = inputFile.Length; //length in bytes
+                long partSize = (long)Math.Ceiling(fileSize / 4.0); //splited into 4 parts
+                byte[] buffer = new byte[partSize];
+
+                for (int i = 1; i <= 4; i++)
+                {
+                    using (var outputFile = new FileStream($"C:\\SoftUni\\C# Advanced\\C# Advanced - May 2019\\Streams, Files And Directories\\Lab\\Resources\\05. Slice File\\Part-{i}.txt", FileMode.Create))
+                    {
+                        int readedBytes = inputFile.Read(buffer, 0, buffer.Length);
+                        outputFile.Write(buffer, 0, readedBytes);
+                    }
+                }
+            }
         }
     }
 }
