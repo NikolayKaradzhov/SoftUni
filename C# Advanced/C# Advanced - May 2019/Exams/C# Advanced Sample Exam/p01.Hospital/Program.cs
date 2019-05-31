62aa6f7cae0dadf9e28645e0a2d39168c75a750f
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-31 20:14:45 +0300

    Update Program.cs

--- C# Advanced/C# Advanced - May 2019/Exams/C# Advanced Sample Exam/p01.Hospital/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Exams/C# Advanced Sample Exam/p01.Hospital/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace p01.Hospital
 {
@@ -6,7 +8,31 @@ namespace p01.Hospital
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            Dictionary<string, int> hospital = new Dictionary<string, int>();
+
+            while (input != "Output")
+            {
+                List<string> tokens = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string department = tokens[0];
+                string doctor = tokens[1];
+                string patient = tokens[2];
+
+
+                input = Console.ReadLine();
+            }
+
+            string inputAgain = Console.ReadLine();
+
+            while (inputAgain != "End")
+            {
+
+
+                inputAgain = Console.ReadLine();
+            }
         }
     }
 }
