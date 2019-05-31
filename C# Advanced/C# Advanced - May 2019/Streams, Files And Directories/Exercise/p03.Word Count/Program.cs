eeb26702f8d1971520ade4df117aff209ac125ee
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-31 22:29:28 +0300

    solved p03

---
+++ C# Advanced/C# Advanced - May 2019/Streams, Files And Directories/Exercise/p03.Word Count/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace p03.Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFilePath = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\Resources\text.txt";
+            string wordsFilePath = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\Resources\words.txt";
+            string actualResultPath = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\p03.Word Count\actualResult.txt";
+            string expectedResultPath = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\p03.Word Count\expectedResult.txt";
+
+            string[] textLines = File.ReadAllLines(textFilePath);
+            string[] words = File.ReadAllLines(wordsFilePath);
+
+            var wordsInfo = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!wordsInfo.ContainsKey(word))
+                {
+                    wordsInfo.Add(word, 0);
+                }
+            }
+
+            foreach (var currentLine in textLines)
+            {
+                string[] wordsSplitted = currentLine
+                .ToLower()
+                .Split(new char[] { ' ', '-', ',', '?', '!', '\'', '.', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var currentWord in wordsSplitted)
+                {
+                    if (wordsInfo.ContainsKey(currentWord))
+                    {
+                        wordsInfo[currentWord]++;
+                    }
+                }
+            }
+
+            foreach (var kvp in wordsInfo)
+            {
+                File.AppendAllText(actualResultPath, $"{kvp.Key} - {kvp.Value}{Environment.NewLine}");
+            }
+
+            var ordered = wordsInfo.OrderByDescending(x => x.Value);
+
+            foreach (var kvp in ordered)
+            {
+                File.AppendAllText(expectedResultPath, $"{kvp.Key} - {kvp.Value}{Environment.NewLine}");
+            }
+        }
+    }
+}
