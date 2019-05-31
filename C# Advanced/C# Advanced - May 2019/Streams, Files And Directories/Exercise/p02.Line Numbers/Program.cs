37e608e199c252a3867e1cfbd15154448ee78e42
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-05-31 21:48:50 +0300

    solve p02

---
+++ C# Advanced/C# Advanced - May 2019/Streams, Files And Directories/Exercise/p02.Line Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace p02.Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFilePath = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\Resources\text.txt";
+            string outputFilePath = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\p02.Line Numbers\output.txt";
+
+            string[] allTextLines = File.ReadAllLines(textFilePath);
+            var lineCounter = 1;
+
+            foreach (var currentLine in allTextLines)
+            {
+                int letterCount = currentLine.Count(char.IsLetter);
+                int puncSignsCount = currentLine.Count(char.IsPunctuation);
+                File.AppendAllText(outputFilePath, $"Line {lineCounter}: {currentLine} ({letterCount})({puncSignsCount})" +
+                    $"{Environment.NewLine}");
+                lineCounter++;
+            }
+        }  
+    }
+}
