80f413afc31c02b5e0468dbe04e0518b0a3f9f8f
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-01 15:22:14 +0300

    p06

---
+++ C# Advanced/C# Advanced - May 2019/Streams, Files And Directories/Exercise/p06.Zip And Extract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace p06.Zip_And_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string picPath = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\Resources";
+            string destinationDir = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\p06.Zip And Extract\result.zip";
+            string extractDir = @"C:\SoftUni\C# Advanced\C# Advanced - May 2019\Streams, Files And Directories\Exercise\p06.Zip And Extract\ExtractDir\";
+
+
+            ZipFile.CreateFromDirectory(picPath, destinationDir);
+
+            ZipFile.ExtractToDirectory(destinationDir, extractDir);
+        }
+    }
+}
