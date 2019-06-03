f4647f32df0cfb48bf373020cb2fd766459190ca
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-03 21:01:07 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Lab/p01.Sort Even Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace p01.Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
