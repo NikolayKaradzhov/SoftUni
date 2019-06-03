08fc9c8d2a29b8e381e3a47d1936506dc7724d52
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-03 21:10:45 +0300

    p03 done

--- C# Advanced/C# Advanced - May 2019/Functional Programming/Lab/p02.Sum Numbers/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Lab/p02.Sum Numbers/Program.cs
@@ -14,7 +14,6 @@ namespace p02.Sum_Numbers
 
             Console.WriteLine(numbers.Count());
             Console.WriteLine(numbers.Sum());
-
         }
         public static Func<string, int> Parse = n => int.Parse(n);
     }
