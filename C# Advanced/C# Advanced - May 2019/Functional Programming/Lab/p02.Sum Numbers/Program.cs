eb3f0e660725c58b60a968b1abafc8bc20ead252
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-03 21:01:07 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Lab/p02.Sum Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace p02.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(Parse)
+                .ToArray();
+
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+
+        }
+        public static Func<string, int> Parse = n => int.Parse(n);
+    }
+}
