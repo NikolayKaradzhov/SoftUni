cea2f8bf2f65a528c5d31f5785afefd428f97874
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-06-04 14:36:48 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p01.Action Point/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p01.Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
