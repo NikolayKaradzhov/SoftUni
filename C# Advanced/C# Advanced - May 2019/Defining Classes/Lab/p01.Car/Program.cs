632fc4f6c9d083c25960699af925bbfd755d8e29
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-06-04 18:03:29 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Defining Classes/Lab/p01.Car/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p01.Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
