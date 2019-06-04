16725ad072b2d0914377ac89c6d7f5c799483af4
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-06-04 16:34:05 +0300

    p03 done

---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p03.Custom Min Function/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace p03.Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> printNumber = x => Console.WriteLine(x);  
+
+            int[] numbersInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> selectMinNumber = numbers =>
+            {
+                int minNumber = numbers.Min();
+                return minNumber;
+            };
+
+            int minValue = selectMinNumber(numbersInput);
+            printNumber(minValue);
+        }
+    }
+}
