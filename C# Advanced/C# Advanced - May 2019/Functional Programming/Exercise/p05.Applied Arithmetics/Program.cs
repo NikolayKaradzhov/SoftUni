99ad9b65721b5de389d33f183284a4530e7e5a47
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-05 20:25:05 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p05.Applied Arithmetics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace p05.Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+            Func<int, int> addFunction = x => x += 1;
+            Func<int, int> multiplyFunction = x => x *= 2;
+            Func<int, int> substractFunction = x => x -= 1;
+
+            Action<int[]> printAllNumbers = numbersToPrint => 
+                    Console.WriteLine(string.Join(" ", numbersToPrint));
+
+            while (command != "end")
+            {
+                if (command == "add")
+                {
+                    numbers = numbers.Select(addFunction).ToArray();
+                }
+                else if (command == "multiply")
+                {
+                    numbers = numbers.Select(multiplyFunction).ToArray();
+                }
+                else if (command == "subtract")
+                {
+                    numbers = numbers.Select(substractFunction).ToArray();
+                }
+                else if (command == "print")
+                {
+                    printAllNumbers(numbers);
+                    break;
+                }
+
+                command = Console.ReadLine(); 
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p06.Reverse And Exclude/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p06.Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
