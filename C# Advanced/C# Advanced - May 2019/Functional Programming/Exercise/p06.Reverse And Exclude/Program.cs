06ff330470a862bd511c442b0440fd39a6dcf41a
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-05 21:56:17 +0300

    .

--- C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p06.Reverse And Exclude/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p06.Reverse And Exclude/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace p06.Reverse_And_Exclude
 {
@@ -6,7 +8,30 @@ namespace p06.Reverse_And_Exclude
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int divisionNumber = int.Parse(Console.ReadLine());
+
+            Func<int, int, bool> isDivisible = (number, division)
+               => number % division == 0;
+
+            Action<int[]> printReverse = numbersToPrint
+                => Console.WriteLine(string.Join(" ", numbersToPrint.Reverse()));
+
+            List<int> printNumbers = new List<int>(numbers);
+
+            foreach (var number in numbers)
+            {
+                if (isDivisible(number, divisionNumber))
+                {
+                    printNumbers.Remove(number);
+                } 
+            }
+
+            printReverse(printNumbers.ToArray());
         }
     }
 }
