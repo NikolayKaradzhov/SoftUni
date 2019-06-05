2cde8e90aaec6aa6d35afd32f2dbac50a69cac0a
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-05 22:25:39 +0300

    .

--- C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p07.Predicate for Names/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p07.Predicate for Names/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace p07.Predicate_for_Names
 {
@@ -6,7 +8,13 @@ namespace p07.Predicate_for_Names
     {
         static void Main(string[] args)
         {
-            
+            int length = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names.Where(n => n.Length <= length)));
         }
     }
 }
