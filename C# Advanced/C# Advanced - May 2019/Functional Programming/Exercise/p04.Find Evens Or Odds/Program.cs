b6ffc8cc90ac7293bfde4f45d90179c6f5f00b80
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-05 20:25:05 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p04.Find Evens Or Odds/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p04.Find_Evens_Or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bounds = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int lowerBound = bounds[0];
+            int upperBound = bounds[1];
+
+            List<int> numbers = new List<int>();
+
+            string commandType = Console.ReadLine();
+
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> isEven = number => 
+                number % 2 == 0;
+
+            Predicate<int> isOdd = number => 
+                number % 2 != 0;
+
+            Action<List<int>> printNumbers = outputNumbers =>
+                Console.WriteLine(string.Join(" ", outputNumbers));
+
+            if (commandType == "even")
+            {
+                numbers = numbers
+                    .Where(x => isEven(x))
+                    .ToList();
+            }
+            else if (commandType == "odd")
+            {
+                numbers = numbers
+                    .Where(x => isOdd(x))
+                    .ToList();
+            }
+
+            printNumbers(numbers);
+        }
+    }
+}
