05c2cf5477073b1e4da7b11ed71192e088951748
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-05 21:56:17 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p12.TriFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace p12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split();
+
+                /*Each name(string)
+                 *  |
+                    |  */  
+            Func<string, int, bool> isLarger = (name, charsLenght)
+                => name.Sum(x => x) >= charsLenght;
+                   /* |
+                      |
+                  Get the sum of the ascii symbols in name(name.Sum)
+                     */
+
+            Func<string[], Func<string, int, bool>, string> nameFilter =
+                (inputNames, isLargerFilter)
+                => inputNames.FirstOrDefault(x => isLargerFilter(x, length));
+
+            string resultName = nameFilter(names, isLarger);
+
+            Console.WriteLine(resultName);
+        }
+    }
+}
