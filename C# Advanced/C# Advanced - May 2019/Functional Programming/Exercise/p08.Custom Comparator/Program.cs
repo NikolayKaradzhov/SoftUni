c65e933f7d452a3159052ff26a88997bc89335b5
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-05 22:19:41 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Functional Programming/Exercise/p08.Custom Comparator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p08.Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
