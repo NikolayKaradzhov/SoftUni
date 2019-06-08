3927f95d679c0ddadd01037679be0b987272405c
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-08 15:57:03 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Defining Classes/Exercise/p02.Creating Constructors/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            Person personTwo = new Person();
+
+            personOne.Name = "Aleks";
+            personOne.Age = 24;
+
+            personTwo.Name = "Niki";
+            personTwo.Age = 28;
+
+
+        }
+    }
+}
