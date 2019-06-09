dc91fe8441a61c133f65d5508df95fd1d2ab9531
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-09 17:31:24 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Defining Classes/Exercise/p05.DateModifier/DateModifier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        private DateTime firstDate;
+        private DateTime secondDate;
+
+        public DateModifier()
+        {
+            FirstDate = firstDate;
+            SecondDate = secondDate;
+        }
+
+        public DateModifier(DateTime firstDate, DateTime secondDate)
+        {
+            this.FirstDate = firstDate;
+            this.SecondDate = secondDate;
+        }
+
+        public DateTime FirstDate
+        {
+            get
+            {
+                return this.firstDate;
+            }
+            set
+            {
+                this.firstDate = value;
+            }
+        }
+
+        public DateTime SecondDate
+        {
+            get
+            {
+                return this.secondDate;
+            }
+            set
+            {
+                this.secondDate = value;
+            }
+        }
+
+        public int ShowDifferenceBetweenTwoDates()
+        {
+            int difference = Math.Abs((firstDate - secondDate).Days);
+
+            return difference;
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Defining Classes/Exercise/p05.DateModifier/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DateModifier dates = new DateModifier
+                (DateTime.Parse(Console.ReadLine()), DateTime.Parse(Console.ReadLine()));
+
+            var difference = dates.ShowDifferenceBetweenTwoDates();
+
+            Console.WriteLine(difference);
+        }
+    }
+}
