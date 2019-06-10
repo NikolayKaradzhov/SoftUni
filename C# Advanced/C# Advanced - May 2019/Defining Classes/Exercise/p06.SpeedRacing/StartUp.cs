e9ee86927be768bdad87248241278351dfab24cc
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-06-10 15:29:38 +0300

    p06 done

---
+++ C# Advanced/C# Advanced - May 2019/Defining Classes/Exercise/p06.SpeedRacing/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p06.SpeedRacing
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int carsCount = int.Parse(Console.ReadLine());
+            List<Car> carList = new List<Car>();
+
+            for (int i = 0; i < carsCount; i++)
+            {
+                string[] carsInput = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carsInput[0];
+                double fuelAmount = double.Parse(carsInput[1]);
+                double fuelConsumptionFor1Km = double.Parse(carsInput[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionFor1Km);
+
+                carList.Add(car);
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] tokens = input
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string carModel = tokens[1];
+                double amountOfKilometers = double.Parse(tokens[2]);
+                int indexOfCurrentCar = carList.IndexOf(carList.Find(x => x.Model == carModel));
+
+                carList[indexOfCurrentCar].Drive(amountOfKilometers);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.Distance}");
+            }
+        }
+    }
+}
