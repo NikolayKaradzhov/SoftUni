8ac18f961973907e760aa169c40fae72d0a7f07e
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-10 22:59:49 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Defining Classes/Exercise/p11.PokemonTrainer/Trainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p11.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name, Pokemon collectionOfPokemon)
+        {
+            this.Name = name;
+            this.CollectionOfPokemon.Add(collectionOfPokemon);
+        }
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> CollectionOfPokemon { get; set; } = new List<Pokemon>();   
+    }
+}
