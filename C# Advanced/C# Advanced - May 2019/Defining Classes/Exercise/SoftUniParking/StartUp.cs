653fc0dcef63a6daddaaad17f6ad2dff92b6b278
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-12 23:06:42 +0300

    :+1:

---
+++ C# Advanced/C# Advanced - May 2019/Defining Classes/Exercise/SoftUniParking/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var parking = new Parking(0);
+
+            var car = new Car("Skoda", "Fabia", 65, "CC1856BG");
+            var car2 = new Car("Audi", "A3", 110, "EB8787MN");
+
+            System.Console.WriteLine(car.ToString());
+
+            Console.WriteLine(parking.AddCar(car));
+            Console.WriteLine(parking.AddCar(car));
+
+            Console.WriteLine(parking.RemoveCar("CC1856BG"));
+
+            Console.WriteLine(parking.Count);
+
+        }
+    }
+}
