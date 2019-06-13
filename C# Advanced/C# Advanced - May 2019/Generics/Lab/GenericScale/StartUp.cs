de876f375eb61336b0086a3ac1f5ab530fbe19d1
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-13 21:52:31 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Generics/Lab/GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var scale = new EqualityScale<string>("5", "5");
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Generics/Lab/p01.Box/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> elements;
+
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        public int Count => elements.Count;
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            T indexOfLastElement = this.elements[this.Count - 1];
+            elements.RemoveAt(Count - 1);
+
+            return indexOfLastElement;
+        }
+    }
+}
