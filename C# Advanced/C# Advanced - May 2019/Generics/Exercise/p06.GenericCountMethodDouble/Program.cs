0fb423efbbe64da35191c558bb9be5f2cd46fca7
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-15 15:32:38 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Generics/Exercise/p06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace p06.GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<double> myBox = new Box<double>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                myBox.Add(input);
+            }
+
+            double comparedNumber = double.Parse(Console.ReadLine());
+
+            myBox.Compare(comparedNumber);
+
+            int result = myBox.CountGreater;
+
+            Console.WriteLine(result);
+        }
+    }
+}
