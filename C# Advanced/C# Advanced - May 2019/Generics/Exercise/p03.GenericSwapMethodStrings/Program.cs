eaee01bcfb289881aba345c4414fe4bee285ea2c
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-15 14:50:39 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Generics/Exercise/p03.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace p03.GenericSwapMethodStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Box<string> myBox = new Box<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+
+                myBox.Add(input);
+            }
+
+            int[] indexesToSwap = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexesToSwap[0];
+            int secondIndex = indexesToSwap[1];
+
+            myBox.SwapIndexes(firstIndex, secondIndex);
+
+            var result = myBox.ToString();
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Generics/Exercise/p04.GenercSwapMethodInt/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p04.GenercSwapMethodInt
+{
+    class Box<TItem>
+    {
+        private List<TItem> box;
+
+        public Box()
+        {
+            box = new List<TItem>();
+        }
+
+        public void Add(TItem item)
+        {
+            box.Add(item);
+        }
+
+        public void SwapIndexes(int firstIndex, int secondIndex)
+        {
+            TItem tempValue = this.box[firstIndex];
+
+            this.box[firstIndex] = this.box[secondIndex];
+            this.box[secondIndex] = tempValue;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var number in box)
+            {
+                sb.AppendLine($"{number.GetType().FullName}: {number}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
