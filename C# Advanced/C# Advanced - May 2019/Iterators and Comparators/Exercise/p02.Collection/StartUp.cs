a0c5eaa59d9e107ee1eaaf8f3288a5bb11c25137
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-17 22:01:55 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Iterators and Comparators/Exercise/p02.Collection/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace p02.Collection
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine()
+                .Split()
+                .Skip(1)
+                .ToArray();
+            ListyIterator<string> list = new ListyIterator<string>(data);
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (InvalidOperationException e)
+                        {
+
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "PrintAll":
+                        Console.WriteLine(string.Join(" ", list));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - May 2019/Iterators and Comparators/Exercise/p05.ComparingObjects/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p05.ComparingObjects
+{
+    class Person
+    {
+    }
+}
