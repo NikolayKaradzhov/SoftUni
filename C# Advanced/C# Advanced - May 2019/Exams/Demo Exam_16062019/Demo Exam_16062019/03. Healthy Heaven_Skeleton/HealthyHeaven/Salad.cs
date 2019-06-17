e322216d74afd6dea57d8e4af036ab9f878adeb8
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-06-17 11:43:45 +0300

    :100:

--- C# Advanced/C# Advanced - May 2019/Exams/Demo Exam_16062019/Demo Exam_16062019/03. Healthy Heaven_Skeleton/HealthyHeaven/Salad.cs
+++ C# Advanced/C# Advanced - May 2019/Exams/Demo Exam_16062019/Demo Exam_16062019/03. Healthy Heaven_Skeleton/HealthyHeaven/Salad.cs
@@ -12,17 +12,15 @@ namespace HealthyHeaven
         public Salad(string name)
         {
             this.Name = name;
-            products = new List<Vegetable>();
+            this.products = new List<Vegetable>();
         }
-        public string Name { get; set; }
-
-        public int Calories { get; set; }
+        public string Name { get; private set; }
 
         public int Count => this.products.Count();
 
         public int GetTotalCalories()
         {
-            int maxCalories = 0;
+            /*int maxCalories = 0;
 
             foreach (var salad in products)
             {
@@ -30,6 +28,8 @@ namespace HealthyHeaven
             }
 
             return maxCalories;
+			*/
+			return this.products.Sum(p => p.Calories);
         }
 
         public void Add(Vegetable product)
@@ -44,8 +44,17 @@ namespace HealthyHeaven
 
         public override string ToString()
         {
-            return $"* Salad {this.Name} is { GetTotalCalories() } calories and have {this.Count} products:{Environment.NewLine}" +
-                $"{string.Join(Environment.NewLine, products)}";
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(
+                $"* Salad {this.Name} is {this.GetTotalCalories()} calories and have {this.GetProductCount()} products:");
+
+            foreach (var item in this.products)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
