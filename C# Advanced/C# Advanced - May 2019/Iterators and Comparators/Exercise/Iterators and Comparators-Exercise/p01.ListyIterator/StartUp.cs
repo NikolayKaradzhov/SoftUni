393d5ac6973584f22fb082de8098ace951b654a4
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-17 21:47:44 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Iterators and Comparators/Exercise/Iterators and Comparators-Exercise/p01.ListyIterator/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace p01.ListyIterator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine()
+                .Split()
+                .Skip(1)
+                .ToArray();
+            ListyIterator<string> list = new ListyIterator<string>(data);
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (InvalidOperationException e)
+                        {
+
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
