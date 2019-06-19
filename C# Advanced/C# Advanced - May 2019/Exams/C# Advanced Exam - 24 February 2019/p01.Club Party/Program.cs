39faea6c3193767b8166ee5805212b051b53dcbd
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-06-19 16:41:19 +0300

    Update Program.cs

--- C# Advanced/C# Advanced - May 2019/Exams/C# Advanced Exam - 24 February 2019/p01.Club Party/Program.cs
+++ C# Advanced/C# Advanced - May 2019/Exams/C# Advanced Exam - 24 February 2019/p01.Club Party/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace p01.Club_Party
 {
@@ -6,7 +7,52 @@ namespace p01.Club_Party
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int maxCapacity = int.Parse(Console.ReadLine());
+            int currentCapacity = 0;
+
+            string[] input = Console.ReadLine()
+                .Split();
+
+            Stack<string> inputHallsAndPeople = new Stack<string>(input);
+            Queue<string> halls = new Queue<string>();
+            List<int> peopleInHall = new List<int>();
+
+            while (inputHallsAndPeople.Count != 0)
+            {
+                string currentInput = inputHallsAndPeople.Pop();
+
+                bool isNumeric = Int32.TryParse(currentInput, out int currentPeople);
+
+                if (!isNumeric)
+                {
+                    halls.Enqueue(currentInput);
+                }
+                else
+                {
+                    if (halls.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    if (currentCapacity + currentPeople < maxCapacity)
+                    {
+                        peopleInHall.Add(currentPeople);
+                        currentCapacity += currentPeople;
+                    }
+                    else
+                    {
+                        if (halls.Count > 0)
+                        {
+                            var currentHall = halls.Dequeue();
+
+                            Console.WriteLine($"{currentHall} -> {string.Join(", ", peopleInHall)}");
+
+                            peopleInHall.Clear();
+                            currentCapacity = 0;
+                        }
+                    }
+                }
+            }
         }
     }
 }
