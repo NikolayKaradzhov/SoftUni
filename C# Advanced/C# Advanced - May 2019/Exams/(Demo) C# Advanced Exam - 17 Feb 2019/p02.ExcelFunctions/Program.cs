f6c1e182120d0dd7a0aba6cdea320502bc71b233
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-20 21:45:51 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Exams/(Demo) C# Advanced Exam - 17 Feb 2019/p02.ExcelFunctions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p02.ExcelFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
