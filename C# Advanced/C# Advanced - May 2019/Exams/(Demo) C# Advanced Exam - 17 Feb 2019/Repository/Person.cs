a5f92c4c7fe3ea47b023f683d5ebb03a9bbf2957
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-20 21:23:50 +0300

    .

---
+++ C# Advanced/C# Advanced - May 2019/Exams/(Demo) C# Advanced Exam - 17 Feb 2019/Repository/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class Person
+    {
+        public Person(string name, int age, DateTime birthDate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.BirthDate = birthDate;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
