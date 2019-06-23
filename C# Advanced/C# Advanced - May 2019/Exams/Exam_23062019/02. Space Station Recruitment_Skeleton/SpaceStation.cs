3a9e6b0d43a5218e964718a6919e14ed385f9e35
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-06-23 13:53:42 +0300

    Exam 300/300

---
+++ C# Advanced/C# Advanced - May 2019/Exams/Exam_23062019/02. Space Station Recruitment_Skeleton/SpaceStation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStationRecruitment
+{
+    public class SpaceStation
+    {
+        private List<Astronaut> data;
+
+        public SpaceStation(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+
+            this.data = new List<Astronaut>();
+        }
+
+        public int Count => this.data.Count();
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public void Add(Astronaut astronaut)
+        {
+            int currentCapacity = this.data.Count();
+
+            if (currentCapacity < this.Capacity)
+            {
+                this.data.Add(astronaut);
+            }
+        }
+
+        public bool Remove(string astronautName)
+        {
+            Astronaut astronaut = data.FirstOrDefault(n => n.Name == astronautName);
+
+            if (data.Contains(astronaut))
+            {
+                data.Remove(astronaut);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Astronaut GetOldestAstronaut()
+        {
+            int oldesAstronaut = this.data.Max(a => a.Age);
+
+            return this.data.FirstOrDefault(a => a.Age == oldesAstronaut);
+        }
+
+        public Astronaut GetAstronaut(string name)
+        {
+            return this.data.FirstOrDefault(a => a.Name == name);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Astronauts working at Space Station {this.Name}:");
+
+            foreach (Astronaut astronaut in this.data)
+            {
+                sb.Append($"{astronaut}{Environment.NewLine}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
