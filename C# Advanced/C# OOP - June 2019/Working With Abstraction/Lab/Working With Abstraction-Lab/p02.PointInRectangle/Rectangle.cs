cbac03b23aaee92a8b801689dcdde8750a058d58
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-07-01 21:28:09 +0200

    .

---
+++ C# Advanced/C# OOP - June 2019/Working With Abstraction/Lab/Working With Abstraction-Lab/p02.PointInRectangle/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p02.PointInRectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            this.TopLeft = topLeft;
+            this.BottomRight = bottomRight;
+        }
+
+        public Point TopLeft { get; set; }
+
+        public Point BottomRight { get; set; }
+
+        public bool Contais (Point point)
+        {
+            return point.X >= TopLeft.X &&
+                   point.X <= BottomRight.X &&
+                   point.Y >= TopLeft.Y &&
+                   point.Y <= BottomRight.Y;
+        }
+    }
+}           
