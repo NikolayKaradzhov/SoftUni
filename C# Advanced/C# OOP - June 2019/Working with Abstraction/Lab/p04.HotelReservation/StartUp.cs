c1565df7a0eef7c597a4370aa2f1f6003989d5f1
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-06 17:54:50 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Working with Abstraction/Lab/p04.HotelReservation/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p04.HotelReservation
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+ 
+        }
+    }
+}
