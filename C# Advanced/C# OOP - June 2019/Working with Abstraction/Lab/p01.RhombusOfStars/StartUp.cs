9dca520e016c97b89c10e5cc5438e22681e53c21
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-06 17:22:14 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Working with Abstraction/Lab/p01.RhombusOfStars/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace p01.RhombusOfStars
+{ 
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= size; i++)
+            {
+                PrintRow(size, i);
+            }
+
+            for (int i = size - 1; i >= 1; i--)
+            {
+                PrintRow(size, i);
+            }
+        }
+
+        private static void PrintRow(int size, int i)
+        {
+            for (int row = 1; row <= size - i; row++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C# Advanced/C# OOP - June 2019/Working with Abstraction/Lab/p04.HotelReservation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p04.HotelReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
