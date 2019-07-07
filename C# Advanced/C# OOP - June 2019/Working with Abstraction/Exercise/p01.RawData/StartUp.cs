6f765dd4b755be7b89da10edea4a94a2f7b1c0b8
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-07 15:18:41 +0300

    refacturing process

---
+++ C# Advanced/C# OOP - June 2019/Working with Abstraction/Exercise/p01.RawData/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p01.RawData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Runner engine = new Runner();
+            
+            engine.Run();
+        }
+    }
+}
