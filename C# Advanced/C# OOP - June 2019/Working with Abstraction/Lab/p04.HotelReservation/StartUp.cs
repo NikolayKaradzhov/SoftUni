6362f4ab50100f9cfd95b7e8e8035514f6aebe30
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-07 13:31:28 +0300

    .

--- C# Advanced/C# OOP - June 2019/Working with Abstraction/Lab/p04.HotelReservation/StartUp.cs
+++ C# Advanced/C# OOP - June 2019/Working with Abstraction/Lab/p04.HotelReservation/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace p04.HotelReservation
 {
@@ -7,7 +8,23 @@ namespace p04.HotelReservation
     {
         public static void Main()
         {
- 
+            List<string> inputParameters = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            double pricePerDay = double.Parse(inputParameters[0]);
+            int numberOfDays = int.Parse(inputParameters[1]);
+            Season season = (Season)Enum.Parse(typeof(Season), inputParameters[2]);
+            DiscountType discountType = DiscountType.None;
+
+            if (inputParameters.Count == 4)
+            {
+                discountType = (DiscountType) Enum.Parse(typeof(DiscountType), inputParameters[3]);
+            }
+
+            PriceCalculator priceCalculator = new PriceCalculator(pricePerDay, numberOfDays, season, discountType);
+
+            Console.WriteLine($"{priceCalculator.CalculatePrice():F2}");
         }
     }
 }
