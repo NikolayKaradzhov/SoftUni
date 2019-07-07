20b2679009853f8741620a02584c71bd942e9f72
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-07 15:18:41 +0300

    refacturing process

---
+++ C# Advanced/C# OOP - June 2019/Working with Abstraction/Exercise/p01.RawData/Car.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace p01.RawData
+{
+    public class Car
+    {
+        public Car(string model,
+            int engineSpeed,
+            int enginePower,
+            int cargoWeight,
+            string cargoType,
+            params Tire[] tires)
+        {
+            this.Model = model;
+            this.EngineSpeed = engineSpeed;
+            this.EnginePower = enginePower;
+            this.CargoWeight = cargoWeight;
+            this.CargoType = cargoType;
+            this.Tires = tires;
+        }
+        public string Model { get; set; }
+        public int EngineSpeed { get; set; }
+        public int EnginePower { get; set; }
+        public int CargoWeight { get; set; }
+        public string CargoType { get; set; }
+        public Tire[] Tires { get; set; }
+    }
+}
