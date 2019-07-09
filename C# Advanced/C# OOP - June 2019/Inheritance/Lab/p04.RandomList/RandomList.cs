7bcdbfbed929edf9f7a9b93b2fe1fd01af2efcae
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-07-09 18:04:26 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Lab/p04.RandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rnd = new Random();
+
+            int index = rnd.Next(0, this.Count);
+            string randomString = this[index];
+            this.Remove(randomString);
+            return randomString;
+        }
+    }
+}
