812159c6ba59d2500674362c1e310530d10dfe74
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-07-09 18:04:26 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Lab/p04.RandomList/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomRandomList
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            RandomList randomList = new RandomList { "1", "2", "3", "4", "5", "432", "432423", "asd", "pesho" };
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Lab/p05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomStack
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange(params string[] inputs)
+        {
+            foreach (var item in inputs)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        }
+    }
+}
