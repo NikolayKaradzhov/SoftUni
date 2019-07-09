2448fa91aa11087c48e97a8ca7713f8c82a78336
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-07-09 17:33:33 +0300

    p03

---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Lab/p03.HierarchicalInheritance/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace Farm
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+
+        }
+    }
+}
