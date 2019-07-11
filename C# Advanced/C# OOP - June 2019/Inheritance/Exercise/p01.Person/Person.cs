f37b4829014984cde26996b3f0c5a5f14f7cbd54
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-07-11 14:31:06 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Exercise/p01.Person/Person.cs
@@ -0,0 +1,45 @@
+﻿namespace P01.Person
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be a negative value");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Name: {this.Name}, Age: {this.Age}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
