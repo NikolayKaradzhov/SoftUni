3ed1638972114c38ea1b4e95ab01782e54c09372
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-07-11 14:31:06 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Exercise/p01.Person/Child.cs
@@ -0,0 +1,10 @@
+﻿namespace P01.Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+        }
+    }
+}
---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Exercise/p01.Person/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace P01.Person
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child firstChild = new Child(name, age);
+
+            Console.WriteLine(firstChild);
+        }
+    }
+}
