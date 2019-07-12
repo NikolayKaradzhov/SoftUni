cb9ac7b592e3ee743fbeaf04a0ae5743f153fbfa
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-12 23:07:19 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Exercise/p05.Restaurant/Foods/MainDishes/Fish.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant.Foods.MainDishes
+{
+    public class Fish : MainDish
+    {
+        private const double DefaultFishGrams = 22;
+
+        public Fish(string name, decimal price, double grams) 
+            : base(name, price, DefaultFishGrams)
+        {
+        }
+    }
+}
