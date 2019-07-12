e3761198f91fcc2c3dc00a97c7cd6855d9951305
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-12 23:07:19 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Inheritance/Exercise/p05.Restaurant/Foods/Desserts/Cake.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant.Foods.Desserts
+{
+    public class Cake : Dessert
+    {
+        private const double DefaultCakeGrams = 250;
+        private const double DefaultCakeCalories = 1000;
+        private const decimal DefaultCakePrice = 5m;
+
+        public Cake(string name) 
+            : base(name, DefaultCakePrice, DefaultCakeGrams, DefaultCakeCalories)
+        {
+        }
+    }
+} 
