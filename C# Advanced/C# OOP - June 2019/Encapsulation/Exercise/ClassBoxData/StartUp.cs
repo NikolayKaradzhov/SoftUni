514919a47326cf8d6ae23bcba36c6c740966cc67
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-07-15 14:42:21 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Encapsulation/Exercise/ClassBoxData/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassBoxData
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():F2}");
+
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():F2}");
+
+                Console.WriteLine($"Volume - {box.CalculateVolume():F2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
