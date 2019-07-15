b4a5d1cd911dd07ccf27aa4a476ca4414f34c05c
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-16 00:00:50 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Encapsulation/Exercise/PizzaCalories/Constraints/LengthConstraints.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaCalories.Constraints
+{
+    public class LengthConstraints
+    {
+        public const int PizzaNameMaxLength = 15;
+
+        public const int MaxToppingsCount = 10;
+
+        public const decimal MaxToppingWeight = 50;
+
+        public const decimal MinToppingWeight = 1;
+
+        public const decimal MinDoughWeight = 1;
+
+        public const decimal MaxDoughWeight = 200;
+    }
+}
