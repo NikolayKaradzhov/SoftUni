2c04266c3668aa7ee2a6e80c50dfcc5e47811e35
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-16 21:24:09 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Interfaces and Abstraction/Exercise/Ferrari/Ferrari.cs
@@ -0,0 +1,35 @@
+﻿namespace Ferrari
+{
+    using System.Text;
+
+    public class Ferrari : IDrive
+    {
+        public Ferrari(string driverName)
+        {
+            this.DriverName = driverName;
+        }
+
+        public string DriverName { get; set; }
+
+        public string Model => "488-Spider";
+
+        public string Brakes()
+        {
+            return "Brakes!";
+        }
+
+        public string Gas()
+        {
+            return "Gas!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Model}/{this.Brakes()}/{this.Gas()}/{this.DriverName}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
