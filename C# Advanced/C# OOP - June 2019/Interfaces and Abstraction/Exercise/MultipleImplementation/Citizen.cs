3ed1c516e7944c238c2cb3b88a80f255ba93b01d
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-16 21:02:59 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Interfaces and Abstraction/Exercise/MultipleImplementation/Citizen.cs
@@ -0,0 +1,21 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        public Citizen(string name, int age, string id, string birthDate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.BirthDate = birthDate;
+            this.Id = id;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string BirthDate { get; set; }
+
+        public string Id { get; set; }
+    }
+}
