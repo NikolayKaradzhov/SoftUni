dcb17d3beb2bfec0291883eb8207c784f5c76703
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-07-17 22:40:03 +0300

    BirthdayCelebrations

---
+++ C# Advanced/C# OOP - June 2019/Interfaces and Abstraction/Exercise/BirthdayCelebrations/Pet.cs
@@ -0,0 +1,28 @@
+﻿namespace BirthdayCelebrations
+{
+    public class Pet : IBirthable
+    {
+        private string name;
+        private string birthDate;
+
+        public Pet(string name, string birthDate)
+        {
+            this.Name = name;
+            this.Birthdate = birthDate;
+        }
+
+        public string Name
+        {
+            get => this.name;
+
+            private set => this.name = value;
+        }
+
+        public string Birthdate
+        {
+            get => this.birthDate;
+
+            private set => this.birthDate = value;
+        }
+    }
+}
