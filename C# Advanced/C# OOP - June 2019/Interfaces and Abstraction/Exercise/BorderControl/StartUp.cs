3bc6c09ee71c6be32022bf43608a89d016967734
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-07-17 22:12:54 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Interfaces and Abstraction/Exercise/BorderControl/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace BorderControl
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                List<string> idList = new List<string>();
+
+                while (input != "End")
+                {
+                    string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    if (tokens.Length == 3)
+                    {
+                        string citizenName = tokens[0];
+                        int age = int.Parse(tokens[1]);
+                        string id = tokens[2];
+
+                        idList.Add(id);
+
+                        Citizen citizen = new Citizen(citizenName, age, id);
+                    }
+                    else if (tokens.Length == 2)
+                    {
+                        string robotModel = tokens[0];
+                        string id = tokens[1];
+
+                        idList.Add(id);
+
+                        Robot robot = new Robot(robotModel, id);
+                    }
+
+                    input = Console.ReadLine();
+                }
+
+                string fakeId = Console.ReadLine();
+
+                foreach (var id in idList)
+                {
+                    if (id.EndsWith(fakeId))
+                    {
+                        Console.WriteLine(id);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
