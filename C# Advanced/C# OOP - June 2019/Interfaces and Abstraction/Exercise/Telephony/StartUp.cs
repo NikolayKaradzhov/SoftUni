64b102fd3af6bb6fb6db1c1011959ad4b8d445e9
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-07-17 16:19:37 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Interfaces and Abstraction/Exercise/Telephony/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace Telephony
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string numbersInput = Console.ReadLine();
+            string sitesInput = Console.ReadLine();
+
+            var phoneNumbers = numbersInput
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var webAddress = sitesInput
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            foreach (var phoneNumber in phoneNumbers)
+            {
+                try
+                {
+                    SmartPhone smartPhone = new SmartPhone { PhoneNumber = phoneNumber };
+                    Console.WriteLine(smartPhone.CallNumbers());
+                }
+                catch (ArgumentException message)
+                {
+                    Console.WriteLine(message.Message);
+                }
+            }
+
+            foreach (var address in webAddress)
+            {
+                try
+                {
+                    SmartPhone smartPhone = new SmartPhone { WebAddress = address };
+                    Console.WriteLine(smartPhone.BrowseSites());
+                }
+                catch (ArgumentException message)
+                {
+                    Console.WriteLine(message.Message);
+                }
+            }
+        }
+    }
+}
