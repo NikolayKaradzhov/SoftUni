4244a7e93b2f261c93f2e1c5d3a0debcccb5c72d
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-07-17 22:12:54 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Interfaces and Abstraction/Exercise/BorderControl/Citizen.cs
@@ -0,0 +1,33 @@
+﻿namespace BorderControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Citizen : Border
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age, string id)
+            : base(id)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public int Age
+        {
+            get => this.age;
+
+            private set => this.age = value;
+        }
+
+        public string Name
+        {
+            get => this.name;
+
+            private set => this.name = value;
+        }
+    }
+}
