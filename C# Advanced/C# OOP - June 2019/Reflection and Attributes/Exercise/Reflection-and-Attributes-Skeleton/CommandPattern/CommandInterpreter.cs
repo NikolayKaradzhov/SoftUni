cbd76e45b9773c0002ffe28ea1a3d4baa774d88d
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-07-26 15:23:13 +0300

    CommandPattern

---
+++ C# Advanced/C# OOP - June 2019/Reflection and Attributes/Exercise/Reflection-and-Attributes-Skeleton/CommandPattern/CommandInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandItems = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = commandItems[0] + "Command";
+
+            string[] items = commandItems.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            Type[] allTypes = assembly.GetTypes();
+
+            Type typeCommand = allTypes.FirstOrDefault(t => t.Name == commandName);
+
+            object instance = Activator.CreateInstance(typeCommand);
+
+            ICommand command = (ICommand) instance;
+
+            return command.Execute(items);
+        }
+    }
+}
---
+++ C# Advanced/C# OOP - June 2019/Reflection and Attributes/Exercise/Reflection-and-Attributes-Skeleton/CommandPattern/Core/Engine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices.ComTypes;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(this.commandInterpreter.Read(input));
+        }
+    }
+}
