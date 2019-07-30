50b5e3867cff38ca2ced9811c915210b2a704b66
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-30 22:48:45 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Commands/AttackMonsterCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MuOnline.Core.Commands
+{
+    class AttackMonsterCommand
+    {
+    }
+}
--- C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Factories/MonsterFactory.cs
+++ C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Factories/MonsterFactory.cs
@@ -1,13 +1,30 @@
-﻿using MuOnline.Core.Factories.Contracts;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using MuOnline.Core.Factories.Contracts;
 using MuOnline.Models.Monsters.Contracts;
 
 namespace MuOnline.Core.Factories
 {
     public class MonsterFactory : IMonsterFactory
     {
-        public IMonster Create(string monsterType)
+        public IMonster Create(string monsterTypeName)
         {
-            throw new System.NotImplementedException();
+            var monsterTypeNameLowerCase = monsterTypeName.ToLower();
+
+            var monsterType = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .FirstOrDefault(m => m.Name.ToLower() == monsterTypeNameLowerCase);
+
+            if (monsterType == null)
+            {
+                throw new ArgumentNullException("Invalid monster type!");
+            }
+
+            var monster = (IMonster)Activator.CreateInstance(monsterType);
+
+            return monster;
         }
     }
 }
