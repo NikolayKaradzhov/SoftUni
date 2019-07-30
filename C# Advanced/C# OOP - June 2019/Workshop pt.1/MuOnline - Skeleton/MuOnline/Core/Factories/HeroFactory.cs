ae13e9099b3c6abdfa8da8e667a4ecab1911388f
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-07-30 16:36:06 +0300

    Update HeroFactory.cs

--- C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Factories/HeroFactory.cs
+++ C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Factories/HeroFactory.cs
@@ -1,6 +1,30 @@
-﻿namespace MuOnline.Core.Factories
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using MuOnline.Core.Factories.Contracts;
+using MuOnline.Models.Heroes.HeroContracts;
+
+namespace MuOnline.Core.Factories
 {
-    public class HeroFactory
+    public class HeroFactory : IHeroFactory
     {
+        public IHero Create(string heroType, string username)
+        {
+            var heroName = heroType.ToLower();
+
+            var type = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .FirstOrDefault(h => h.Name.ToLower() == heroName);
+
+            if (type == null)
+            {
+                throw new ArgumentNullException("Invalid hero type!");
+            }
+
+            var hero = (IHero)Activator.CreateInstance(type, username);
+
+            return hero;
+        }
     }
 }
