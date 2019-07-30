43c242b490f826713c9c78dfa8320483565b6bd8
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-07-30 21:30:25 +0300

    AddMonsterCommand

--- C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Commands/AddMonsterCommand.cs
+++ C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Commands/AddMonsterCommand.cs
@@ -1,7 +1,31 @@
 ﻿namespace MuOnline.Core.Commands
 {
-    public class AddMonsterCommand 
+    using Contracts;
+    using Factories.Contracts;
+    using Models.Monsters.Contracts;
+    using Repositories.Contracts;
+    public class AddMonsterCommand : ICommand
     {
-        
+        private const string successfullMessage = "Successfully created monster: {0}";
+
+        private readonly IRepository<IMonster> monsterRepository;
+        private readonly IMonsterFactory monsterFactory;
+
+        public AddMonsterCommand(IRepository<IMonster> monsterRepository, IMonsterFactory monsterFactory)
+        {
+            this.monsterRepository = monsterRepository;
+            this.monsterFactory = monsterFactory;
+        }
+
+        public string Execute(string[] inputArgs)
+        {
+            string monsterTypeName = inputArgs[0];
+
+            var monster = monsterFactory.Create(monsterTypeName);
+
+            this.monsterRepository.Add(monster);
+
+            return string.Format(successfullMessage, monster.GetType().Name);
+        }
     }
 }
