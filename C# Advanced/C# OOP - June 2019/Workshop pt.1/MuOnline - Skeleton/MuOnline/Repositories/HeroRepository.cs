4c5703af28b8b5ec0bccd50ab81ba4f937927d80
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-07-30 16:13:20 +0300

    .

--- C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Repositories/HeroRepository.cs
+++ C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Repositories/HeroRepository.cs
@@ -1,7 +1,55 @@
-﻿namespace MuOnline.Repositories
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MuOnline.Models.Heroes.HeroContracts;
+using MuOnline.Repositories.Contracts;
+
+namespace MuOnline.Repositories
 {
-    public class HeroRepository
+    public class HeroRepository : IRepository<IHero>
     {
-        
+        private readonly List<IHero> heroes;
+
+        public HeroRepository()
+        {
+            this.heroes = new List<IHero>();
+        }
+
+        public IReadOnlyCollection<IHero> Repository
+            => this.heroes.AsReadOnly();
+
+        public void Add(IHero hero)
+        {
+            if (hero == null) 
+            {
+                throw new ArgumentNullException("Hero cannot be null!");
+            }
+
+            this.heroes.Add(hero);
+        }
+
+        public bool Remove(IHero hero)
+        {
+            if (hero == null)
+            {
+                throw new ArgumentNullException("Hero cannot be null!");
+            }
+
+            bool isRemoved = this.heroes.Remove(hero);
+
+            return isRemoved;
+        }
+
+        public IHero Get(string hero)
+        {
+            var targetHero = this.heroes.FirstOrDefault(h => ((IIdentifiable) h).Username == hero);
+
+            if (targetHero == null)
+            {
+                throw new ArgumentNullException("Hero cannot be null!");
+            }
+
+            return targetHero;
+        }
     }
 }
--- C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Repositories/MonsterRepository.cs
+++ C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Repositories/MonsterRepository.cs
@@ -1,7 +1,57 @@
-﻿namespace MuOnline.Repositories
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MuOnline.Models.Heroes.HeroContracts;
+using MuOnline.Models.Monsters.Contracts;
+using MuOnline.Repositories.Contracts;
+
+namespace MuOnline.Repositories
 {
-    public class MonsterRepository
+    public class MonsterRepository : IRepository<IMonster>
     {
-       
+        private readonly List<IMonster> monsters;
+
+        public MonsterRepository()
+        {
+            this.monsters = new List<IMonster>();
+        }
+
+        public IReadOnlyCollection<IMonster> Repository
+            => this.monsters.AsReadOnly();
+
+        public void Add(IMonster monster)
+        {
+            if (monster == null)
+            {
+                throw new ArgumentNullException("Monster cannot be null!");
+            }
+
+            this.monsters.Add(monster);
+        }
+
+        public bool Remove(IMonster monster)
+        {
+            if (monster == null)
+            {
+                throw new ArgumentNullException("Monster cannot be null");
+            }
+
+            bool isMonsterRemoved = this.monsters.Remove(monster);
+
+            return isMonsterRemoved;
+        }
+
+        public IMonster Get(string monster)
+        {
+            var targetMonster = this.monsters.FirstOrDefault(m => m.GetType().Name == monster);
+
+            if (targetMonster == null)
+            {
+                throw new ArgumentNullException("Monster cannot be null!");
+            }
+
+            return targetMonster;
+        }
     }
 }
