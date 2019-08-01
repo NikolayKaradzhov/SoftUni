cb331033e2dd9cddc921b382bc2e69de7831ae14
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-08-01 19:08:26 +0300

    .

--- C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Commands/AttackMonsterCommand.cs
+++ C# Advanced/C# OOP - June 2019/Workshop pt.1/MuOnline - Skeleton/MuOnline/Core/Commands/AttackMonsterCommand.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MuOnline.Core.Commands.Contracts;
+using MuOnline.Models.Heroes.HeroContracts;
+using MuOnline.Models.Monsters.Contracts;
+using MuOnline.Repositories.Contracts;
 
 namespace MuOnline.Core.Commands
 {
-    class AttackMonsterCommand
+    public class AttackMonsterCommand : ICommand
     {
+        private const string commandMessage = "{0} is dead!";
+
+        private readonly IRepository<IHero> heroRepository; 
+        private readonly IRepository<IMonster> monsterRepository;
+
+        public AttackMonsterCommand(IRepository<IHero> heroRepository, IRepository<IMonster> monsterRepository)
+        {
+            this.heroRepository = heroRepository;
+            this.monsterRepository = monsterRepository;
+        }
+
+        public string Execute(string[] inputArgs)
+        {
+            string heroUsername = inputArgs[0];
+            string monsterName = inputArgs[1];
+
+            var hero = this.heroRepository.Get(heroUsername);
+            var monster = this.monsterRepository.Get(monsterName);
+
+            var heroAttackPoints = hero.TotalAttackPoints;
+            var monsterAttackPoints = monster.AttackPoints;
+
+            while (hero.IsAlive && monster.IsAlive)
+            {
+                hero.TakeDamage(monsterAttackPoints);
+                var experience = monster.TakeDamage(heroAttackPoints);
+
+                ((IProgress)hero).AddExperience(experience);
+            }
+
+            return string.Format(commandMessage, hero.IsAlive ? monster.GetType().Name : heroUsername);
+        }
     }
 }
