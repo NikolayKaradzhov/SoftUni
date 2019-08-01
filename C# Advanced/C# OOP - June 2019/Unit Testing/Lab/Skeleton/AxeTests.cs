3138df7b55693a34d9741fa60bd74a44a5ac3ca1
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-08-01 22:47:23 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Unit Testing/Lab/Skeleton/AxeTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private Axe axe;
+        private Dummy dummyTarget;
+
+        [SetUp]
+        public void CreateAxe()
+        {
+            axe = new Axe(100, 1);
+        }
+
+        [SetUp]
+        public void CreateDummy()
+        {
+            dummyTarget = new Dummy(100, 100);
+        }
+
+        [Test]
+        public void TestAxeLosingDurability()
+        {
+            axe.Attack(dummyTarget);
+
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestAttackingWithBrokenWeapon()
+        {
+            Axe axe = new Axe(1, 1);
+            Dummy dummy = new Dummy(100, 100);
+
+            Assert.That(() => axe.Attack(dummy),
+                Throws.InvalidOperationException.With.Message.EqualTo("Axe is broken."));
+        }
+    }
+}
---
+++ C# Advanced/C# OOP - June 2019/Unit Testing/Lab/Skeleton/DummyTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Skeleton
+{
+    [TestFixture]
+    public class DummyTest
+    {
+        private Dummy dummy;
+
+        [SetUp]
+        public void CreateDummy()
+        {
+            dummy = new Dummy(100, 100);
+        }
+
+        [Test]
+        public void DummyLoosesHealthIfAttacked()
+        {
+            dummy.TakeAttack(50);
+
+            Assert.That(dummy.Health, Is.EqualTo(50));
+        }
+    }
+}
