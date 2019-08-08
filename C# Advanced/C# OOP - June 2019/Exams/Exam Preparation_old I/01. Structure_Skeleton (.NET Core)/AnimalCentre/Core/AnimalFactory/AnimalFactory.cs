dcb2f87d2733c78e6977837076543ae4fcfa8d6b
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-08-08 22:42:14 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Exams/Exam Preparation_old I/01. Structure_Skeleton (.NET Core)/AnimalCentre/Core/AnimalFactory/AnimalFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using AnimalCentre.Models.Contracts;
+
+namespace AnimalCentre.Core.AnimalFactory
+{
+    public class AnimalFactory : IAnimalFactory
+    {
+        public IAnimal CreateAnimal(string type, string name, int energy, int happiness, int procedureTime)
+        {
+            var animalType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == type);
+
+            var animal = (IAnimal)Activator.CreateInstance(animalType, name, energy, happiness, procedureTime);
+
+            return animal;
+        }
+    }
+}
--- C# Advanced/C# OOP - June 2019/Exams/Exam Preparation_old I/01. Structure_Skeleton (.NET Core)/AnimalCentre/Models/Hotels/Hotel.cs
+++ C# Advanced/C# OOP - June 2019/Exams/Exam Preparation_old I/01. Structure_Skeleton (.NET Core)/AnimalCentre/Models/Hotels/Hotel.cs
@@ -1,10 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
+using AnimalCentre.Models.Contracts;
 
 namespace AnimalCentre.Models.Hotels
 {
-    class Hotel
+    public class Hotel : IHotel
     {
+        private const int Capacity = 10;
+
+        private readonly Dictionary<string, IAnimal> animals;
+
+        public Hotel()
+        {
+            this.animals = new Dictionary<string, IAnimal>();
+        }
+
+        public IReadOnlyDictionary<string, IAnimal> Animals
+            => this.animals.ToImmutableDictionary();
+
+
+        public void Accommodate(IAnimal animal)
+        {
+            if (animals.Count == Capacity)
+            {
+                throw new InvalidOperationException("Not enough capacity");
+            }
+
+            if (animals.ContainsKey(animal.Name))
+            {
+                throw new ArgumentException($"Animal {animal.Name} already exist");
+            }
+
+            this.animals[animal.Name] = animal;
+        }
+
+        public void Adopt(string animalName, string owner)
+        {
+            if (!animals.ContainsKey(animalName))
+            {
+                throw new ArgumentException($"Animal {animalName} does not exist");
+            }
+
+            animals[animalName].Owner = owner;
+            animals[animalName].IsAdopt = true;
+
+            animals.Remove(animalName);
+        }
     }
 }
