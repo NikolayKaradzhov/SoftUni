dbff68c23d7e97897db11e1c0a759e8156a9a409
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-08-09 15:39:47 +0300

    12643

---
+++ C# Advanced/C# OOP - June 2019/Exams/C# OOP Exam - 14 April 2019/01. Structure_Skeleton (1)/Skeleton/MortalEngines/Entities/BaseMachine.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Entities
+{
+    public abstract class BaseMachine : IMachine
+    {
+        private string name;
+        private IPilot pilot;
+        private IList<string> targets;
+
+        private BaseMachine()
+        {
+            this.targets = new List<string>();
+        }
+
+        public BaseMachine(string name, double attackPoints, double defensePoints, double healthPoints)
+            :this()
+        {
+            this.Name = name;
+            this.AttackPoints = attackPoints;
+            this.DefensePoints = defensePoints;
+            this.HealthPoints = healthPoints;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Machine name cannot be null or empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public IPilot Pilot
+        {
+            get { return this.pilot; }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException("Pilot cannot be null.");
+                }
+
+                this.pilot = value;
+            }
+        }
+
+        public double HealthPoints { get;  set; }
+
+        public double AttackPoints { get; set; }
+
+        public double DefensePoints { get; set; }
+
+        public IList<string> Targets
+        {
+            get { return this.targets; }
+
+            private set { this.targets = value; }
+        }
+
+        public void Attack(IMachine target)
+        {
+            if (target == null)
+            {
+                throw new NullReferenceException("Target cannot be null");
+            }
+
+            double hpDifference = 
+                this.AttackPoints - target.DefensePoints;
+
+            target.HealthPoints -= hpDifference;
+
+            if (target.HealthPoints < 0)
+            {
+                target.HealthPoints = 0;
+            }
+
+            this.targets.Add(target.Name);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string targetsOutput = this.targets.Count > 0 ? String.Join(",", this.targets) : "None";
+
+            sb.AppendLine($"- {this.Name}");
+            sb.AppendLine($" *Type: {this.GetType().Name}");
+            sb.AppendLine($" *Health: {this.HealthPoints:f2}");
+            sb.AppendLine($" *Attack: {this.AttackPoints:f2}");
+            sb.AppendLine($" *Defense: {this.DefensePoints:f2}");
+            sb.AppendLine($" *Targets: {targetsOutput}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/C# OOP - June 2019/Exams/C# OOP Exam - 14 April 2019/01. Structure_Skeleton (1)/Skeleton/MortalEngines/Entities/Pilot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Entities
+{
+    public class Pilot : IPilot
+    {
+        private string _name;
+        private IList<IMachine> machines;
+
+        public Pilot(string name)
+        {
+            this.Name = _name;
+            this.machines = new List<IMachine>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Pilot name cannot be null or empty string.");
+                }
+
+                this._name = value;
+            }
+        }
+
+        public void AddMachine(IMachine machine)
+        {
+            if (machine == null)
+            {
+                throw new NullReferenceException("Null machine cannot be added to the pilot.");
+            }
+
+            this.machines.Add(machine);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Name} - {this.machines.Count} machines");
+
+            foreach (var machine in machines)
+            {
+                sb.AppendLine(machine.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
