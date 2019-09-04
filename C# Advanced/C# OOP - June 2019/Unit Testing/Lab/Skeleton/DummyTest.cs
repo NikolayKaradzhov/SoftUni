eea6ecf6e89d54a28778a8d4e37e01746f977273
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-04 19:24:21 +0300

    .

--- C# Advanced/C# OOP - June 2019/Unit Testing/Lab/Skeleton/DummyTest.cs
+++ C# Advanced/C# OOP - June 2019/Unit Testing/Lab/Skeleton/DummyTest.cs
@@ -20,5 +20,35 @@ namespace Skeleton
 
             Assert.That(dummy.Health, Is.EqualTo(50));
         }
+
+        [Test]
+        public void TestIfDeadDummyThrowsExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(2, 1);
+
+            dummy.TakeAttack(2);
+
+            Assert.That(() => dummy.TakeAttack(0),
+                Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
+        }
+
+        [Test]
+        public void TestIfDeadDummyCanGiveXP()
+        {
+            dummy.TakeAttack(100);
+
+            dummy.GiveExperience();
+
+            Assert.That(() => dummy.GiveExperience(), Is.EqualTo(100));
+        }
+
+        [Test]
+        public void TestAliveDummyCannotGiveXP()
+        {
+            dummy.TakeAttack(99);
+
+            Assert.That(() => dummy.GiveExperience(),
+                Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+        }
     }
 }
