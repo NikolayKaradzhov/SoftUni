c4ebd3c22f8bfadc4fc4ed4d2b816171c22ba2f9
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-05 16:19:35 +0300

    SoftUniRestaurant

---
+++ C# Advanced/C# OOP - June 2019/Exams/Exam Preparation_old II/01. Structure_Skeleton (.NET Core)/Models/Foods/Factories/FoodFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using SoftUniRestaurant.Models.Foods.Contracts;
+
+namespace SoftUniRestaurant.Models.Foods.Factories
+{
+    public class FoodFactory
+    {
+        public IFood CreateFood(string type, string name, decimal price)
+        {
+            Type foodType = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == type);
+
+            IFood food = (IFood)Activator.CreateInstance(foodType, name, price);
+
+            return food;
+        }
+    }
+}
