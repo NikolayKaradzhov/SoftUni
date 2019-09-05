f538f2c36bb0fbe1f4c9f6e088c2a3f6cefc47b4
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-05 16:19:35 +0300

    SoftUniRestaurant

---
+++ C# Advanced/C# OOP - June 2019/Exams/Exam Preparation_old II/01. Structure_Skeleton (.NET Core)/Models/Tables/Factories/TableFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using SoftUniRestaurant.Models.Tables.Contracts;
+
+namespace SoftUniRestaurant.Models.Tables.Factories
+{
+    public class TableFactory
+    {
+        public ITable CreateTable(string type, int tableNumber, int capacity)
+        {
+            ITable table;
+
+            switch (type)
+            {
+                case "Inside": table = new InsideTable(tableNumber,capacity);
+                    break;
+                case "Outside": table = new OutsideTable(tableNumber, capacity);
+                    break;
+                default: table = null;
+                    break;
+            }
+
+            return table;
+        }
+    }
+}
