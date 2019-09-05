09fa56c242714aec014c852f77a7b0369c5a8aec
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-05 16:38:28 +0300

    Update RestaurantController.cs

--- C# Advanced/C# OOP - June 2019/Exams/Exam Preparation_old II/01. Structure_Skeleton (.NET Core)/Core/RestaurantController.cs
+++ C# Advanced/C# OOP - June 2019/Exams/Exam Preparation_old II/01. Structure_Skeleton (.NET Core)/Core/RestaurantController.cs
@@ -1,22 +1,60 @@
-﻿namespace SoftUniRestaurant.Core
+﻿using System.Collections;
+using System.Collections.Generic;
+using SoftUniRestaurant.Models.Drinks.Contracts;
+using SoftUniRestaurant.Models.Drinks.Factories;
+using SoftUniRestaurant.Models.Foods.Contracts;
+using SoftUniRestaurant.Models.Foods.Factories;
+using SoftUniRestaurant.Models.Tables.Contracts;
+using SoftUniRestaurant.Models.Tables.Factories;
+
+namespace SoftUniRestaurant.Core
 {
     using System;
 
     public class RestaurantController
     {
+        private IList<IFood> menu;
+        private IList<IDrink> drinks;
+        private IList<ITable> tables;
+        private FoodFactory foodFactory;
+        private DrinkFactory drinkFactory;
+        private TableFactory tableFactory;
+
+        public RestaurantController(IList<IFood> menu, IList<IDrink> drinks, IList<ITable> tables)
+        {
+            this.menu = new List<IFood>();
+            this.drinks = new List<IDrink>();
+            this.tables = new List<ITable>();
+            this.foodFactory = new FoodFactory();
+            this.drinkFactory = new DrinkFactory();
+            this.tableFactory = new TableFactory();
+        }
+
         public string AddFood(string type, string name, decimal price)
         {
-            throw new NotImplementedException();
+            IFood food = this.foodFactory.CreateFood(type, name, price);
+
+            menu.Add(food);
+
+            return $"Added {name} ({type}) with price {price:f2} to the pool";
         }
 
         public string AddDrink(string type, string name, int servingSize, string brand)
         {
-            throw new NotImplementedException();
+            IDrink drink = this.drinkFactory.CreateDrink(type, name, servingSize, brand);
+
+            drinks.Add(drink);
+
+            return $"Added {name} ({brand}) to the drink pool";
         }
 
         public string AddTable(string type, int tableNumber, int capacity)
         {
-            throw new NotImplementedException();
+            ITable table = this.tableFactory.CreateTable(type, tableNumber, capacity);
+
+            tables.Add(table);
+
+            return $"Added table number {tableNumber} in the restaurant";
         }
 
         public string ReserveTable(int numberOfPeople)
