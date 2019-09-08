b578a4777d53c3dc072b7f44780f93aed98aa4b4
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-08 12:24:09 +0300

    .

---
+++ C# Advanced/C# OOP - June 2019/Exams/C# OOP Demo Exam - 04 August 2019/01. Structure_Skeleton/MXGP/Repositories/Contracts/MotorcycleRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MXGP.Models.Motorcycles.Contracts;
+
+namespace MXGP.Repositories.Contracts
+{
+    public class MotorcycleRepository : IRepository<IMotorcycle>
+    {
+        private readonly IList<IMotorcycle> motorcycles;
+
+        public MotorcycleRepository()
+        {
+            this.motorcycles = new List<IMotorcycle>();
+        }
+
+        public IMotorcycle GetByName(string name)
+            => this.motorcycles.FirstOrDefault(m => m.Model == name);
+
+        public IReadOnlyCollection<IMotorcycle> GetAll()
+            => this.motorcycles.ToList();
+
+        public void Add(IMotorcycle model)
+            => this.motorcycles.Remove(model);
+
+        public bool Remove(IMotorcycle model)
+            => this.motorcycles.Remove(model);
+    }
+}
---
+++ C# Advanced/C# OOP - June 2019/Exams/C# OOP Demo Exam - 04 August 2019/01. Structure_Skeleton/MXGP/Repositories/Contracts/RiderRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MXGP.Models.Motorcycles.Contracts;
+using MXGP.Models.Riders.Contracts;
+
+namespace MXGP.Repositories.Contracts
+{
+    public class RiderRepository : IRepository<IRider>
+    {
+        private readonly IList<IRider> riders;
+
+        public RiderRepository()
+        {
+            this.riders = new List<IRider>();
+        }
+
+        public IRider GetByName(string name)
+            => this.riders.FirstOrDefault(r => r.Name == name);
+
+        public IReadOnlyCollection<IRider> GetAll()
+            => this.riders.ToList();
+
+        public void Add(IRider rider)
+            => this.riders.Remove(rider);
+
+        public bool Remove(IRider rider)
+            => this.riders.Remove(rider);
+    }
+}
