516a4f03f67fad81a214f84ff04b17b127a9d395
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-09 16:35:59 +0300

    MXGP Business logic done!

--- C# Advanced/C# OOP - June 2019/Exams/C# OOP Demo Exam - 04 August 2019/01. Structure_Skeleton/MXGP/Core/ChampionshipController.cs
+++ C# Advanced/C# OOP - June 2019/Exams/C# OOP Demo Exam - 04 August 2019/01. Structure_Skeleton/MXGP/Core/ChampionshipController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using MXGP.Core.Contracts;
 using MXGP.Models.Motorcycles;
 using MXGP.Models.Motorcycles.Contracts;
+using MXGP.Models.Races;
+using MXGP.Models.Races.Contracts;
 using MXGP.Models.Riders;
 using MXGP.Models.Riders.Contracts;
 using MXGP.Repositories.Contracts;
@@ -12,11 +16,13 @@ namespace MXGP.Core
     {
         private readonly IRepository<IRider> ridersRepository;
         private readonly IRepository<IMotorcycle> motorcycleRepository;
+        private readonly IRepository<IRace> raceRepository;
 
         public ChampionshipController()
         {
             this.ridersRepository = new RiderRepository();
             this.motorcycleRepository = new MotorcycleRepository();
+            this.raceRepository = new RaceRepository();
         }
 
         public string CreateRider(string riderName)
@@ -59,22 +65,89 @@ namespace MXGP.Core
 
         public string CreateRace(string name, int laps)
         {
-            throw new NotImplementedException();
+            IRace race = raceRepository.GetByName(name);
+
+            if (race != null)
+            {
+                throw new InvalidOperationException($"Race {race.Name} is already created.");
+            }
+
+            race = new Race(name, laps);
+
+            raceRepository.Add(race);
+
+            return $"Race {race.Name} is created.";
         }
 
         public string AddRiderToRace(string raceName, string riderName)
         {
-            throw new NotImplementedException();
+            IRace race = this.raceRepository.GetByName(raceName);
+            IRider rider = this.ridersRepository.GetByName(riderName);
+
+            if (race == null)
+            {
+                throw new InvalidOperationException($"Race {race.Name} could not be found.");
+            }
+
+            if (rider == null)
+            {
+                throw new InvalidOperationException($"Rider {rider.Name} could not be found.");
+            }
+
+            race.AddRider(rider);
+
+            return $"Rider {rider.Name} added in {race.Name} race.";
         }
 
         public string AddMotorcycleToRider(string riderName, string motorcycleModel)
         {
-            throw new NotImplementedException();
+            IRider rider = this.ridersRepository.GetByName(riderName);
+
+            if (rider == null)
+            {
+                throw new InvalidOperationException($"Rider {rider.Name} could not be found.");
+            }
+
+            IMotorcycle motorcycle = this.motorcycleRepository.GetByName(motorcycleModel);
+
+            if (motorcycle == null)
+            {
+                throw new InvalidOperationException($"Motorcycle {motorcycle.Model} could not be found.");
+            }
+
+            rider.AddMotorcycle(motorcycle);
+
+            return $"Rider {rider.Name} received motorcycle {motorcycle.Model}.";
         }
 
         public string StartRace(string raceName)
         {
-            throw new NotImplementedException();
+            IRace race = this.raceRepository.GetByName(raceName);
+
+            if (race == null)
+            {
+                throw new InvalidOperationException($"Race {race.Name} could not be found.");
+            }
+
+            if (race.Riders.Count < 3)
+            {
+                throw new InvalidOperationException($"Race {race.Name} cannot start with less than 3 participants.");
+            }
+
+            var riders = race.Riders
+                .OrderByDescending(x => x.Motorcycle.CalculateRacePoints(race.Laps))
+                .Take(3)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Rider {riders[0]} wins {race.Name} race.");
+            sb.AppendLine($"Rider {riders[1]} is second in {race.Name} race.");
+            sb.AppendLine($"Rider {riders[2]} is third in {race.Name} race.");
+
+            this.raceRepository.Remove(race);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
