fd8eb5ea438b30eedaff33d4ea7ef24299464060
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-18 22:09:03 +0300

    Update Databases Introduction. Data Definition And Datatypes-Exercise.sql

--- C# DB/MS SQL - September 2019/Databases Introduction. Data Definition And Datatypes/Exercise/Databases Introduction. Data Definition And Datatypes-Exercise.sql
+++ C# DB/MS SQL - September 2019/Databases Introduction. Data Definition And Datatypes/Exercise/Databases Introduction. Data Definition And Datatypes-Exercise.sql
@@ -253,3 +253,201 @@ VALUES
     (123, 123333, 123455, GETDATE(), GETDATE()),
     (12, 111111, 333333, GETDATE(), GETDATE()),
     (23, 222222, 555555, GETDATE(), GETDATE())
+
+
+--15.Hotel Database--
+CREATE DATABASE Hotel
+
+--15.Create Hotel Tables--
+CREATE TABLE Employees(
+Id INT PRIMARY KEY IDENTITY (1,1),
+FirstName NVARCHAR(30) NOT NULL,
+LastName NVARCHAR(30) NOT NULL,
+Title NVARCHAR(20),
+Notes NVARCHAR(200)
+)
+
+INSERT INTO
+    Employees(FirstName, LastName, Notes)
+VALUES
+    ('Gosho', 'Petkov', NULL),
+    ('Ivan', 'Ivanov', NULL),
+    ('Georgi', 'Vasilev', NULL)
+
+CREATE TABLE Customers(
+Id INT PRIMARY KEY IDENTITY (1,1),
+AccountNumber INT NOT NULL,
+FirstName NVARCHAR(30) NOT NULL ,
+LastName NVARCHAR(30) NOT NULL ,
+PhoneNumber INT,
+EmergencyName NVARCHAR(30) NOT NULL ,
+EmergencyNumber INT NOT NULL,
+Notes NVARCHAR(30)
+)
+
+INSERT INTO
+    Customers(AccountNumber, FirstName, LastName, EmergencyName, EmergencyNumber)
+VALUES
+    (123312321, 'Vasko', 'Ivanov', '911Emergency', 911),
+    (4234324, 'Gosko', 'Petkov', '112Emergency', 112),
+    (64564646, 'Ivancho', 'Georgiev', '611Call', 611)
+
+
+CREATE TABLE RoomStatus (
+Id INT PRIMARY KEY IDENTITY (1,1),
+RoomStatus NVARCHAR(10) NOT NULL ,
+Notes NVARCHAR(200)
+)
+
+INSERT INTO
+    RoomStatus(RoomStatus)
+VALUES
+    ('Free'),
+    ('Occupied'),
+    ('Half');
+
+CREATE TABLE RoomTypes(
+Id INT PRIMARY KEY IDENTITY (1,1),
+RoomType NVARCHAR(30) NOT NULL,
+Notes NVARCHAR(200)
+)
+
+INSERT INTO
+    RoomTypes(RoomType)
+VALUES
+    ('Single'),
+    ('Double'),
+    ('Triple');
+
+CREATE TABLE BedTypes (
+Id INT PRIMARY KEY IDENTITY (1,1),
+BedType NVARCHAR(30) NOT NULL,
+Notes NVARCHAR(200)
+)
+
+INSERT INTO
+    BedTypes(BedType)
+VALUES
+    ('Single'),
+    ('Double'),
+    ('Tripple');
+
+CREATE TABLE Rooms (
+Id INT PRIMARY KEY IDENTITY (1,1),
+RoomNumber INT NOT NULL,
+RoomType INT FOREIGN KEY REFERENCES RoomTypes(Id),
+BedType INT FOREIGN KEY REFERENCES BedTypes(Id),
+Rate INT CHECK (Rate >= 1 AND Rate <= 5),
+RoomStatus INT FOREIGN KEY REFERENCES RoomStatus(Id),
+Notes NVARCHAR(200)
+)
+
+INSERT INTO
+    Rooms(RoomNumber, RoomType, BedType, RoomStatus)
+VALUES
+    (123, 1, 1, 1),
+    (456, 2, 2, 2),
+    (789, 3, 3, 3)
+
+CREATE TABLE Payments (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    EmployeeId INT FOREIGN KEY REFERENCES Employees(Id),
+    PaymentDate DATE,
+    AccountNumber INT,
+    FirstDateOccupied DATE,
+    LastDateOccupied DATE,
+    TotalDays INT,
+    AmountCharged MONEY,
+    TaxRate MONEY,
+    TaxAmount MONEY,
+    PaymentTotal MONEY,
+    Notes NVARCHAR(200)
+)
+
+INSERT INTO
+    Payments(EmployeeId)
+VALUES
+    (2),
+    (3),
+    (1);
+
+
+CREATE TABLE Occupancies (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    EmployeeId INT FOREIGN KEY REFERENCES Employees(Id),
+    DateOccupied DATE,
+    AccountNumber INT,
+    RoomNumber INT FOREIGN KEY REFERENCES Rooms(Id) ,
+    RateApplied BIT,
+    PhoneCharge BIT,
+    Notes NVARCHAR(200)
+)
+
+INSERT INTO
+    Occupancies(EmployeeId)
+VALUES
+    (2),
+    (1),
+    (3);
+
+
+--19.Basic Select All Fields--
+
+SELECT * FROM Towns;
+SELECT * FROM Departments;
+SELECT * FROM Employees;
+
+
+--20.Basic Select All Fields and Order Them--
+
+SELECT * FROM Towns ORDER BY Name
+SELECT * FROM Departments ORDER BY Name
+SELECT * FROM Employees ORDER BY Salary DESC
+
+--21.Basic Select Some Fields--
+
+SELECT
+    Name
+FROM
+    Towns
+ORDER BY
+    Name;
+
+SELECT
+    Name
+FROM
+    Departments
+ORDER BY
+    Name;
+
+--22.Increase Empleyee Salary--
+
+UPDATE
+    Employees
+SET
+    Salary = Salary + Salary * 0.10;
+SELECT
+    Salary
+FROM
+    Employees;
+SELECT
+    FirstName, LastName, JobTitle, Salary
+FROM
+    Employees
+ORDER BY
+    Salary DESC;
+
+--23.Decrease Tax Rate--
+
+UPDATE
+    Payments
+SET
+    TaxRate -= TaxRate * 0.03;
+SELECT
+    TaxRate
+FROM
+    Payments;
+
+--24.Delete All Records--
+
+TRUNCATE TABLE Occupancies
