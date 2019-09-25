a5cc8ee3b31556b45b4163e67a47fc870c65f395
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-25 21:15:01 +0300

    Create Built-in Functions - Exercise.sql

---
+++ C# DB/MS SQL - September 2019/Built-in Functions/Exercise/Built-in Functions - Exercise.sql
@@ -0,0 +1,131 @@
+--01. Find Names of All Employees by First Name.01--
+
+SELECT
+    FirstName, LastName
+FROM
+    Employees
+WHERE
+    FirstName LIKE 'SA%'
+
+
+--02.Find Names of All Employees by Last Name.02--
+
+SELECT
+    FirstName, LastName
+FROM
+    Employees
+WHERE
+    LastName LIKE '%ei%'
+
+
+--03. Find First Names of All Employees.03--
+
+SELECT
+    FirstName
+FROM
+    Employees
+WHERE
+    DepartmentID IN (3, 10)
+AND
+    DATEPART(YEAR , HireDate) BETWEEN 1995 AND 2005
+
+
+
+--04.Find All Employees Except Engineers.04--
+
+SELECT
+    FirstName, LastName
+FROM
+    Employees
+WHERE
+    JobTitle NOT LIKE '%engineer%'
+
+
+--05. Find Towns with Name Length.05--
+
+SELECT
+    [Name]
+FROM
+    Towns
+WHERE
+    LEN(Name) BETWEEN 5 AND 6
+ORDER BY [Name] ASC
+
+
+--06. Find Towns Starting With.06--
+
+SELECT
+    TownID, [Name]
+FROM
+    Towns
+WHERE
+    [Name] LIKE 'M%' OR [Name] LIKE 'K%' OR [Name] LIKE 'B%' OR [Name] LIKE 'E%'
+ORDER BY
+    [Name] ASC
+
+
+
+--07. Find Towns Not Starting With.07--
+
+SELECT
+    TownID, [Name]
+FROM
+    Towns
+WHERE
+    [Name] NOT LIKE 'R%' AND [Name] NOT LIKE 'B%' AND [Name] NOT LIKE 'D%'
+ORDER BY
+    [Name] ASC
+
+
+--08. Create View Employees Hired After.08--
+
+CREATE VIEW
+    V_EmployeesHiredAfter2000
+AS
+SELECT
+    FirstName, LastName
+FROM
+    Employees
+WHERE
+    DATEPART(YEAR, HireDate) > 2000
+
+
+--09. Length of Last Name.09--
+
+SELECT
+    FirstName, LastName
+FROM
+    Employees
+WHERE
+    LEN(LastName) = 5
+
+
+--10. Rank Employees by Salary.10--
+
+SELECT
+    EmployeeID, FirstName, LastName, Salary,
+DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+FROM
+    Employees
+WHERE
+    Salary BETWEEN 10000 AND 50000
+ORDER BY
+    Salary DESC
+
+
+--11. Find All Employees with Rank 2--
+
+
+
+
+--12. Countries Holding 'A'--
+
+SELECT
+    CountryName, IsoCode
+FROM
+    Countries
+WHERE
+    CountryName LIKE '%a%a%a%'
+ORDER BY
+    IsoCode ASC
+
