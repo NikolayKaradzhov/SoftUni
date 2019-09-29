cb01a81babf7cd44a83de0aa03def2b05c0926e4
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-09-29 16:29:01 +0300

    Data Aggregation Update

--- C# DB/MS SQL - September 2019/Data Aggredation/Data Aggregation - Exercise.sql
+++ C# DB/MS SQL - September 2019/Data Aggredation/Data Aggregation - Exercise.sql
@@ -89,18 +89,32 @@ ORDER BY
 --09. Age Groups.09--
 SELECT [Ages].AgeGroup, COUNT(Ages.AgeGroup)
     FROM
-(
-SELECT
-    CASE
-        WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
-        WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
-        WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
-        WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
-        WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
-        WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
-        ELSE '[61+]'
-    END AS [AgeGroup]
-FROM  WizzardDeposits) AS Ages
+        (
+            SELECT
+                CASE
+                    WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+                    WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+                    WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+                    WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+                    WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+                    WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+                ELSE '[61+]'
+            END AS [AgeGroup]
+    FROM  WizzardDeposits) AS Ages
 GROUP BY
     Ages.AgeGroup
+
+
+--10.First Letter.10--
+
+SELECT
+    LEFT(FirstName, 1) AS [FirstLetter]
+FROM
+    WizzardDeposits
+WHERE
+    DepositGroup = 'Troll Chest'
+GROUP BY
+    LEFT(FirstName, 1)
+ORDER BY
+    FirstLetter ASC
 
