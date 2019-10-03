1ebf83b7df502e3e8382c2614610ae13e5677149
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-10-03 13:51:04 +0300

    Update Table Relations - Exercise.sql

--- C# DB/MS SQL - September 2019/Table Relations/Exercise/Table Relations - Exercise.sql
+++ C# DB/MS SQL - September 2019/Table Relations/Exercise/Table Relations - Exercise.sql
@@ -80,3 +80,122 @@ SELECT *
 
 --03.Many-To-Many Relationship.03--
 
+CREATE DATABASE ManyToMany
+
+USE ManyToMany
+
+CREATE TABLE Students
+(
+    StudentID INT PRIMARY KEY IDENTITY NOT NULL,
+    Name NVARCHAR(30) NOT NULL
+)
+
+INSERT
+  INTO Students(Name)
+VALUES ('Mila'),
+       ('Toni'),
+       ('Ron')
+
+CREATE TABLE Exams
+(
+    ExamID INT PRIMARY KEY NOT NULL,
+    Name NVARCHAR(30) NOT NULL
+)
+
+INSERT
+  INTO Exams(ExamID, Name)
+VALUES (101, 'SpringMVC'),
+       (102, 'Neo4j'),
+       (103, 'Oracle 11g')
+
+CREATE TABLE StudentsExams
+(
+    StudentID INT FOREIGN KEY REFERENCES Students (StudentID),
+    ExamID INT FOREIGN KEY REFERENCES Exams (ExamID),
+    CONSTRAINT PK_StudentsExams PRIMARY KEY (StudentID, ExamID)
+)
+
+INSERT
+  INTO StudentsExams(StudentID, ExamID)
+VALUES (1, 101),
+       (1, 102),
+       (2, 101),
+       (3, 103),
+       (2, 102),
+       (2, 103)
+
+
+SELECT *
+  FROM StudentsExams AS se
+           JOIN Students s ON se.StudentId = s.StudentID
+           JOIN Exams e ON se.ExamId = e.ExamID
+
+
+--04.Self-Referencing.04--
+
+CREATE DATABASE SelfReferencing
+
+USE SelfReferencing
+
+CREATE TABLE Teachers
+(
+    TeacherID INT PRIMARY KEY IDENTITY (101,1) NOT NULL,
+    Name NVARCHAR(30) NOT NULL,
+    ManagerID INT FOREIGN KEY REFERENCES Teachers (TeacherID)
+)
+
+INSERT
+  INTO Teachers(Name, ManagerID)
+VALUES ('John', NULL),
+       ('Maya', 106),
+       ('Silvia', 106),
+       ('Ted', 105),
+       ('Mark', 101),
+       ('Greta', 101)
+
+
+--05.Online Store Database.05--
+
+CREATE DATABASE OnlineStore
+
+USE OnlineStore
+
+CREATE TABLE Cities
+(
+    CityID INT PRIMARY KEY IDENTITY NOT NULL,
+    Name NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Customers
+(
+    CustomerID INT PRIMARY KEY IDENTITY NOT NULL ,
+    Name NVARCHAR(50) NOT NULL,
+    Birthday DATE NOT NULL,
+    CityID INT FOREIGN KEY REFERENCES Cities(CityID) NOT NULL
+)
+
+CREATE TABLE Orders
+(
+    OrderID INT PRIMARY KEY IDENTITY NOT NULL ,
+    CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID) NOT NULL
+)
+
+CREATE TABLE ItemTypes
+(
+    ItemTypeID INT PRIMARY KEY IDENTITY NOT NULL,
+    Name NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Items
+(
+    ItemID INT PRIMARY KEY IDENTITY NOT NULL ,
+    Name NVARCHAR(50) NOT NULL ,
+    ItemTypeID INT FOREIGN KEY REFERENCES ItemTypes(ItemTypeID) NOT NULL
+)
+
+CREATE TABLE OrderItems
+(
+    OrderID INT FOREIGN KEY REFERENCES Orders(OrderID) NOT NULL ,
+    ItemID INT FOREIGN KEY REFERENCES Items(ItemID) NOT NULL ,
+    CONSTRAINT PK_OrderItems PRIMARY KEY (OrderID, ItemID)
+)
