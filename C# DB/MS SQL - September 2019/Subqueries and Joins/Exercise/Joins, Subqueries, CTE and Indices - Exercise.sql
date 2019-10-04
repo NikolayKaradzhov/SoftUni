de39c3b8858aee58773a77fd51585d0974f241b3
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-10-04 10:09:08 +0300

    Create Joins, Subqueries, CTE and Indices - Exercise.sql

---
+++ C# DB/MS SQL - September 2019/Subqueries and Joins/Exercise/Joins, Subqueries, CTE and Indices - Exercise.sql
@@ -0,0 +1,104 @@
+--01.Employee Address--
+
+
+
+
+--02.Addresses with Towns--
+
+
+
+
+
+--03. Sales Employees--
+
+
+
+
+
+--04. Employee Departments--
+
+
+
+
+
+--05. Employees Without Projects--
+
+
+
+
+
+--06. Employees Hired After--
+
+
+
+
+
+--07. Employees With Project--
+
+
+
+
+
+--08. Employee 24--
+
+
+
+
+
+--09. Employee Manager--
+
+
+
+
+
+--10. Employees Summary--
+
+
+
+
+
+--11. Min Average Salary--
+
+
+
+
+
+--12. Highest Peaks in Bulgaria--
+
+
+
+
+
+--13. Count Mountain Ranges--
+
+
+
+
+
+--14. Countries With or Without Rivers--
+
+
+
+
+
+--15. Continents and Currencies--
+
+
+
+
+
+--16. Countries Without any Mountains--
+
+
+
+
+
+--17. Highest Peak and Longest River by Country--
+
+
+
+
+
+--18. Highest Peak Name and Elevation by Country--
+
+
