f440f35fa953cf6645a41aacf0d62ac352bab3ef
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-10-04 11:01:06 +0300

    Update Joins, Subqueries, CTE and Indices - Exercise.sql

--- C# DB/MS SQL - September 2019/Subqueries and Joins/Exercise/Joins, Subqueries, CTE and Indices - Exercise.sql
+++ C# DB/MS SQL - September 2019/Subqueries and Joins/Exercise/Joins, Subqueries, CTE and Indices - Exercise.sql
@@ -12,7 +12,13 @@ ORDER BY AddressID
 
 --02.Addresses with Towns--
 
-
+SELECT
+    TOP 50
+        FirstName, LastName, T.Name, A.AddressText
+FROM Employees AS E
+JOIN Addresses AS A ON E.AddressID = A.AddressID
+JOIN Towns T ON A.TownID = T.TownID
+ORDER BY FirstName, LastName
 
 
 
