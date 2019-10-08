705e9c1fee5b0a11f4cf9f9f787f7afe5e7fa8b5
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-10-08 23:19:41 +0300

    Update Part 1 - SoftUni Database.sql

--- C# DB/MS SQL - September 2019/Database Programmability and Transactions/Exercise/Section I/Part 1 - SoftUni Database.sql
+++ C# DB/MS SQL - September 2019/Database Programmability and Transactions/Exercise/Section I/Part 1 - SoftUni Database.sql
@@ -14,6 +14,7 @@ BEGIN
 END
 
 GO
+
 --02.Employees with Salary Above Number--
 
 CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber(@Salary DECIMAL(18, 4)) AS
@@ -21,11 +22,10 @@ BEGIN
     SELECT FirstName, LastName
       FROM Employees AS e
      WHERE e.Salary >= @Salary
-END
-
-    EXEC usp_GetEmployeesSalaryAboveNumber 40000.22
+END EXEC usp_GetEmployeesSalaryAboveNumber 40000.22
 
 GO
+
 --03.Town Names Starting With--
 
 CREATE PROCEDURE usp_GetTownsStartingWith(@StartLetter VARCHAR(10)) AS
@@ -33,11 +33,10 @@ BEGIN
     SELECT t.Name AS Town
       FROM Towns AS t
      WHERE t.Name LIKE @StartLetter + '%'
-END
-
-    EXEC usp_GetTownsStartingWith b
+END EXEC usp_GetTownsStartingWith b
 
 GO
+
 --04.Employees from Town--
 
 CREATE PROCEDURE usp_GetEmployeesFromTown(@TownName NVARCHAR(20)) AS
@@ -51,62 +50,60 @@ END
 
     EXEC usp_GetEmployeesFromTown Sofia
 
+GO
 
 --05.Salary Level Function--
 
-CREATE FUNCTION ufn_GetSalaryLevel(@Salary DECIMAL(18,4))
-RETURNS NVARCHAR(10)
-AS
-BEGIN
-    DECLARE @result NVARCHAR(20)
+    CREATE FUNCTION ufn_GetSalaryLevel(@Salary DECIMAL(18, 4)) RETURNS NVARCHAR(10) AS
+    BEGIN
+        DECLARE @result NVARCHAR(20)
+        BEGIN
+            IF (@salary < 30000)
+                SET @result = 'Low'
+            ELSE IF (@salary BETWEEN 30000 AND 50000)
+                SET @result = 'Average'
+            ELSE IF (@salary > 50000) SET @result = 'High'
+        END
+        RETURN @result
+    END
 
-        IF (@salary < 30000)
-            SET @result = 'Low'
-        ELSE IF (@salary BETWEEN  30000 AND 50000)
-            SET @result = 'Average'
-        ELSE IF (@salary > 50000)
-            SET @result = 'High'
-    RETURN @result
-END
-
-
+GO
 
 --06.Employees by Salary Level--
 
-CREATE PROCEDURE usp_EmployeesBySalaryLevel (@Level NVARCHAR(20)) AS
-BEGIN
+CREATE PROCEDURE usp_EmployeesBySalaryLevel(@Level NVARCHAR(20)) AS
+ BEGIN
     SELECT FirstName, LastName
-    FROM Employees AS e
-    WHERE dbo.ufn_GetSalaryLevel(Salary) = @Level
-END
+      FROM Employees AS e
+     WHERE dbo.ufn_GetSalaryLevel(Salary) = @Level
+ END
 
 GO
 
 --07.Define Function--
 
-CREATE FUNCTION ufn_IsWordComprised(@setOfLetters NVARCHAR(50), @word NVARCHAR(50))
-RETURNS BIT                         --OFIAS--
+CREATE FUNCTION ufn_IsWordComprised(@setOfLetters NVARCHAR(50), @word NVARCHAR(50)) RETURNS BIT --OFIAS--
 AS
-    BEGIN
-        DECLARE @result BIT
-        DECLARE @count INT = 1
+BEGIN
+    DECLARE @result BIT
+    DECLARE @count INT = 1
 
-        WHILE @count <= LEN(@word)
-        BEGIN
-            DECLARE @currentSymbol NVARCHAR(1) = SUBSTRING(@word, @count, 1)
-                                                          --Sofia--
-            IF CHARINDEX(@currentSymbol, @setOfLetters) > 0
-                BEGIN   --S>o>f>i>a--      --OFIAS--
-                    SET @result = 1
-                    SET @count += 1
-                END
-            ELSE
-                BEGIN
-                    SET @result = 0
-                END
-        END
-        RETURN @result
+    WHILE @count <= LEN(@word) BEGIN
+        DECLARE @currentSymbol NVARCHAR(1) = SUBSTRING(@word, @count, 1)
+        --Sofia--
+        IF CHARINDEX(@currentSymbol, @setOfLetters) > 0
+            BEGIN
+                --S>o>f>i>a--      --OFIAS--
+                SET @result = 1
+                SET @count += 1
+            END
+        ELSE
+            BEGIN
+                SET @result = 0
+            END
     END
+    RETURN @result
+END
 
 
 GO
@@ -114,7 +111,7 @@ GO
 
 --08.Delete Employees and Departments--
 
-CREATE PROCEDURE usp_DeleteEmployeesFromDepartment (@departmentId INT) AS
+CREATE PROCEDURE usp_DeleteEmployeesFromDepartment(@departmentId INT) AS
 BEGIN
 
 END
