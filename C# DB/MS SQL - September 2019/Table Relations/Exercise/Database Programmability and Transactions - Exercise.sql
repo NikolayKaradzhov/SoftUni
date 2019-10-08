da041ace1dc19970555ec635e0a3579eb951c67a
Nikolay Karadzhov <nikoo.karadzhov@gmail.com>
2019-10-08 21:48:05 +0300

    1-4 Done!

---
+++ C# DB/MS SQL - September 2019/Table Relations/Exercise/Database Programmability and Transactions - Exercise.sql
@@ -0,0 +1,52 @@
+--SECTION 1--
+
+--Part 1. Queries for SoftUni Database--
+
+--01.Employees with Salary Above 35000--
+
+USE SoftUni
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000 AS
+BEGIN
+    SELECT FirstName, LastName
+      FROM Employees AS e
+     WHERE Salary > 35000
+END
+
+GO
+--02.Employees with Salary Above Number--
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber(@Salary DECIMAL(18, 4)) AS
+BEGIN
+    SELECT FirstName, LastName
+      FROM Employees AS e
+     WHERE e.Salary >= @Salary
+END
+
+    EXEC usp_GetEmployeesSalaryAboveNumber 40000.22
+
+GO
+--03.Town Names Starting With--
+
+CREATE PROCEDURE usp_GetTownsStartingWith(@StartLetter VARCHAR(10)) AS
+BEGIN
+    SELECT t.Name AS Town
+      FROM Towns AS t
+     WHERE t.Name LIKE @StartLetter + '%'
+END
+
+    EXEC usp_GetTownsStartingWith b
+
+GO
+--04.Employees from Town--
+
+CREATE PROCEDURE usp_GetEmployeesFromTown(@TownName NVARCHAR(20)) AS
+BEGIN
+    SELECT e.FirstName, e.LastName
+      FROM Employees AS e
+               JOIN Addresses AS a ON a.AddressID = e.AddressID
+               JOIN Towns AS t ON t.TownID = a.TownID
+     WHERE @TownName = t.Name
+END
+
+    EXEC usp_GetEmployeesFromTown Sofia
