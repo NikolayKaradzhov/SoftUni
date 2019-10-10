e955c6d7dcc16f8457542637b1f646073153d092
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-10-10 23:01:15 +0300

    Update Part 3 - Diablo Database.sql

--- C# DB/MS SQL - September 2019/Database Programmability and Transactions/Exercise/Section I/Part 3 - Diablo Database.sql
+++ C# DB/MS SQL - September 2019/Database Programmability and Transactions/Exercise/Section I/Part 3 - Diablo Database.sql
@@ -3,4 +3,16 @@
 --Part 3 - Diablo Database--
 
 --13. *Scalar Function: Cash in User Games Odd Rows--
+USE Diablo
 
+CREATE FUNCTION ufn_CashInUsersGames(@gameName NVARCHAR(MAX))
+    RETURNS TABLE
+        AS RETURN
+                            SELECT SUM(Cash) AS SumCash
+                              FROM (SELECT DENSE_RANK() OVER (ORDER BY Cash DESC ) AS RowNumber, ug.Cash
+                                      FROM Games AS g
+                                               JOIN UsersGames AS ug ON ug.GameId = g.Id
+                                     WHERE Name = @gameName) AS Cash
+                             WHERE RowNumber % 2 != 0
+
+SELECT * FROM dbo.ufn_CashInUsersGames('Love in a mist')
