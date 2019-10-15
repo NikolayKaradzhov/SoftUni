e18a1519ced9a2c925a67a981a8e142ce4745496
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-10-16 00:25:26 +0300

    Update Exam Preparation I.sql

--- C# DB/MS SQL - September 2019/Table Relations/Exercise/Exam Preparation I.sql
+++ C# DB/MS SQL - September 2019/Table Relations/Exercise/Exam Preparation I.sql
@@ -177,12 +177,29 @@ SELECT p.Name, p.Seats, COUNT(t.Id) AS [Passengers Count]
 
 --11.Vacation--
 
-CREATE FUNCTION udf_CalculateTickets(@origin NVARCHAR(50), @destination NVARCHAR(50), @peopleCount INT)
-RETURNS DECIMAL(18,2)
+CREATE FUNCTION udf_CalculateTickets(@origin varchar(50), @destination varchar(50), @peopleCount int)
+RETURNS VARCHAR(100)
 AS
 BEGIN
-    IF (@peopleCount <= 0)
-    BEGIN
 
-    END
+IF (@peopleCount <= 0)
+BEGIN
+	RETURN 'Invalid people count!'
+END
+
+DECLARE @tripId INT = (SELECT f.Id FROM Flights AS f
+											  JOIN Tickets AS t ON t.FlightId = f.Id
+											  WHERE Destination = @destination AND Origin = @origin)
+IF (@tripId IS NULL)
+BEGIN
+	RETURN 'Invalid flight!'
+END
+
+DECLARE @ticketPrice DECIMAL(15,2) = (SELECT t.Price FROM Flights AS f
+											  JOIN Tickets AS t ON t.FlightId = f.Id
+											  WHERE Destination = @destination AND Origin = @origin)
+
+DECLARE @totalPrice DECIMAL(15, 2) = @ticketPrice * @peoplecount;
+
+RETURN 'Total price ' + CAST(@totalPrice as VARCHAR(30));
 END
