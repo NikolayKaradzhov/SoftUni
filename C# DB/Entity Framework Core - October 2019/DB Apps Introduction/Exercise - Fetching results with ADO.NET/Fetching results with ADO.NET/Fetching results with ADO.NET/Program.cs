35cb654e0e5c531339079d4b48481395c44c45c3
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-10-22 21:20:29 +0300

    Fetching results with ADO.NET

---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/Fetching results with ADO.NET/Fetching results with ADO.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fetching_results_with_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
