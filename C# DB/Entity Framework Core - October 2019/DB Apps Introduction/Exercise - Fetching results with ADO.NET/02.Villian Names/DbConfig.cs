b122a7ef677d6e3ac98040af5d9a9c3cb3457356
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-10-27 15:27:52 +0200

    02.Done

---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/02.Villian Names/DbConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace _02.Villian_Names
+{
+    public class DbConfig
+    {
+        public const string ConnectionString = @"Server=BGL063;Database=MinionsDB;Integrated Security=true";
+    }
+}
---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/02.Villian Names/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.Villian_Names
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(DbConfig.ConnectionString);
+
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand(Queries.VilliansWithMinions, connection);
+
+                    SqlDataReader reader = (command.ExecuteReader());
+
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            string villianName = (string)reader[0];
+                            int minionsCount = (int)reader[1];
+
+                            Console.WriteLine($"{villianName} - {minionsCount}");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
