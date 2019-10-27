709959952b023577b0bb8949293728bbe5352b48
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-10-27 22:15:26 +0200

    05.Done

---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/05.Change Town Name Casing/Queries.cs
@@ -0,0 +1,14 @@
+﻿namespace _05.Change_Town_Name_Casing
+{
+    public class Queries
+    {
+        public const string UpdateTownQuery = @"UPDATE Towns 
+                                                SET Name = UPPER(Name)
+                                                WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+
+        public const string GetCountryTowns = @"SELECT t.Name
+                                                FROM Towns AS t
+                                                JOIN Countries AS c ON t.CountryCode = c.Id
+                                                WHERE c.Name = @countryName";
+    }
+}
---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/05.Change Town Name Casing/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace _05.Change_Town_Name_Casing
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string countryInput = Console.ReadLine();
+
+            string connectionString = @"Server = BGL063; Database = MinionsDB; Integrated Security = true";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            using (connection)
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(Queries.UpdateTownQuery, connection);
+
+                using (command)
+                {
+                    command.Parameters.AddWithValue("@countryName", countryInput);
+
+                    int countRowsAffected = command.ExecuteNonQuery();
+
+                    Console.WriteLine($"{countRowsAffected} town names were affected.");
+                }
+
+                command = new SqlCommand(Queries.GetCountryTowns, connection);
+
+                using (command)
+                {
+                    command.Parameters.AddWithValue("@countryName", countryInput);
+
+                    List<string> cities = new List<string>();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            cities.Add((string)reader[0]);
+                        }
+                    }
+
+                    if (cities.Count == 0)
+                    {
+                        Console.WriteLine("No town names were affected.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"[{string.Join(", ", cities)}]");
+                    }
+                }
+            }
+        }
+    }
+}
