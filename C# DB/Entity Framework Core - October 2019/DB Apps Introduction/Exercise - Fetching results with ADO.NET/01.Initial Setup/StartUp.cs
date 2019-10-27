547f799c3fdcf6dd910d8f106b2bf1880d214779
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-10-27 14:59:35 +0200

    01. Done

---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/01.Initial Setup/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace Fetching_results_with_ADO.NET
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(DbConfig.ConnectionString);
+
+            using (connection)
+            {
+                try
+                {
+                    connection.Open();
+
+                    foreach (var createTableCommand in DbConfig.CreateTableCommands)
+                    {
+                        SqlCommand createCommand = new SqlCommand(createTableCommand, connection);
+
+                        createCommand.ExecuteNonQuery();
+                    }
+
+                    foreach (var insertCommandStatements in DbConfig.InsertCommands)
+                    {
+                        SqlCommand insertCommand = new SqlCommand(insertCommandStatements, connection);
+
+                        insertCommand.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+    }
+}
