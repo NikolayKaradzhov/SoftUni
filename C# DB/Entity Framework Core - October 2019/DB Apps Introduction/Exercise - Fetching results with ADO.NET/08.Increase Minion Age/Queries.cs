28b688b191d472772da730dc49724b132dc9c15b
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-10-28 16:08:36 +0200

    08.Done

---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/08.Increase Minion Age/Queries.cs
@@ -0,0 +1,11 @@
+﻿namespace _08.Increase_Minion_Age
+{
+    public class Queries
+    {
+        public const string UpdateMinionsQuery = "UPDATE Minions " +
+                                                 "SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1" +
+                                                 "WHERE Id = @Id";
+
+        public const string SelectMinionInformation = "SELECT Name, Age FROM Minions";
+    }
+}
