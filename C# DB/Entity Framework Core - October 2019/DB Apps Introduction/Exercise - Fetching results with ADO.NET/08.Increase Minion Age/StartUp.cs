3d82074c5a7e6a7ce4e0fb626b295d7207c44f45
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-10-28 16:08:36 +0200

    08.Done

---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/08.Increase Minion Age/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace _08.Increase_Minion_Age
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static string connectionString = @"Server = BGL063; Database = MinionsDB; Integrated Security = true";
+        public static SqlConnection connection = new SqlConnection(connectionString);
+
+        public static void Main()
+        {
+            List<int> ids = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            using (connection)
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(Queries.UpdateMinionsQuery, connection);
+
+                using (command)
+                {
+                    foreach (var id in ids)
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+                        command.ExecuteNonQuery();
+                        command.Parameters.Clear();
+                    }
+                }
+
+                SqlCommand newCommand = new SqlCommand(Queries.SelectMinionInformation, connection);
+
+                using (newCommand)
+                {
+                    SqlDataReader reader = newCommand.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string minionName = (string)reader[0];
+                        int minionAge = (int)reader[1];
+
+                        Console.WriteLine($"{minionName} {minionAge}");
+                    }
+                }
+            }
+        }
+    }
+}
