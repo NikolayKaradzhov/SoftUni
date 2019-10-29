a973f00c0d09c941e647d0b7c14ad5793e6db8c2
NikolayKaradzhov <nikoo.karadzhov@gmail.com>
2019-10-29 11:47:01 +0200

    09.Done

---
+++ C# DB/Entity Framework Core - October 2019/DB Apps Introduction/Exercise - Fetching results with ADO.NET/09.Increase Age Stored Procedure/Queries.cs
@@ -0,0 +1,9 @@
+﻿namespace _09.Increase_Age_Stored_Procedure
+{
+    public class Queries
+    {
+        public const string UpdateAgeStoredProc = "CREATE PROC usp_GetOlder @id INT AS UPDATE Minions SET Age += 1 WHERE Id = @id";
+
+        public const string SelectNameAgeQuery = "SELECT Name, Age FROM Minions WHERE Id = @Id";
+    }
+}
