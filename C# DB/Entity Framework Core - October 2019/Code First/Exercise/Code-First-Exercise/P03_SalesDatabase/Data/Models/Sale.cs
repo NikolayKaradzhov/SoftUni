c9679e3cfd289d0597168399a7e1b914f3c12000
Nikolay Karadzhov <N.Karadzhov@printecgroup.com>
2019-11-05 19:55:01 +0200

    Entities Done !

---
+++ C# DB/Entity Framework Core - October 2019/Code First/Exercise/Code-First-Exercise/P03_SalesDatabase/Data/Models/Sale.cs
@@ -0,0 +1,26 @@
+﻿namespace P03_SalesDatabase.Data.Models
+{
+    using System;
+
+    public class Sale
+    {
+        public int SaleId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        //Foreign Key
+        public int ProductId { get; set; }
+        //Navigation Property for navigation into the entities
+        public Product Product { get; set; }
+
+        //Foreign Key
+        public int CustomerId { get; set; }
+        //Navigation Property
+        public Customer Customer { get; set; }
+
+        //Foreign Key
+        public int StoreId { get; set; }
+        //Navigation Property
+        public Store Store { get; set; }
+    }
+}
