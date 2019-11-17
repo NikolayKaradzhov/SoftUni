b32dd74393f4aee3bddbbaac8d1b629b9115b354
Nikolay Karadzhov PC <nikoo.karadzhov@gmail.com>
2019-11-17 11:04:58 +0200

    1,2 done

--- C# DB/Entity Framework Core - October 2019/Advanced Querying/Exercise/BookShop/BookShop/StartUp.cs
+++ C# DB/Entity Framework Core - October 2019/Advanced Querying/Exercise/BookShop/BookShop/StartUp.cs
@@ -1,4 +1,9 @@
-﻿namespace BookShop
+﻿using System;
+using System.Linq;
+using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
 {
     using Data;
     using Initializer;
@@ -11,6 +16,53 @@
             {
                 DbInitializer.ResetDatabase(db);
             }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var input = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .Select(b => new
+                {
+                    b.Title,
+                    b.AgeRestriction
+                })
+                .Where(b => b.AgeRestriction == input)
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldenBooks = context.Books
+                .Select(b => new
+                {
+                    b.Copies,
+                    b.Title,
+                    b.EditionType,
+                    b.BookId
+                })
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in goldenBooks)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
